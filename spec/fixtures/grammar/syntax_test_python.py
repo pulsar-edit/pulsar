f129be1c615e3e5853e9421573e7161e02494fd2
Kevin Stone <kevinastone@gmail.com>
2018-04-22 14:41:12 -0700

    Migrated function syntax checks to atom-grammar-tests

--- spec/fixtures/grammar/syntax_test_python.py
+++ spec/fixtures/grammar/syntax_test_python.py
@@ -5,16 +5,10 @@ def my_func(first, second=False, *third, **forth):
 # <- storage.type.function
 #   ^^^^^^^ entity.name.function
 #          ^ punctuation.definition.parameters.begin
-#           ^^^^^ variable.parameter.function
-#                ^ punctuation.separator.parameters
-#                  ^^^^^^ variable.parameter.function
+#           ^^^^^  ^^^^^^         ^^^^^    ^^^^^ variable.parameter.function
+#                ^             ^       ^ punctuation.separator.parameters
 #                        ^ keyword.operator.assignment
 #                         ^^^^^ constant
-#                              ^ punctuation.separator.parameters
-#                                ^ keyword.operator.unpacking.arguments
-#                                 ^^^^^ variable.parameter.function
-#                                      ^ punctuation.separator.parameters
-#                                        ^^ keyword.operator.unpacking.arguments
-#                                          ^^^^^ variable.parameter.function
+#                                ^       ^^ keyword.operator.unpacking.arguments
 #                                                ^ punctuation.definition.function.begin
     pass
