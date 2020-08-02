e2e931e4446a4648e1d92f24f7f1ee25e9257d97
aminya <aminyahyaabadi74@gmail.com>
2020-08-02 01:51:15 -0500

    use nsfw 2

--- src/path-watcher.js
+++ src/path-watcher.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const { Emitter, Disposable, CompositeDisposable } = require('event-kit');
-const nsfw = require('@atom/nsfw');
+const nsfw = require('nsfw');
 const watcher = require('@atom/watcher');
 const { NativeWatcherRegistry } = require('./native-watcher-registry');
 
