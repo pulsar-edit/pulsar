f5b0ef37473cc6d219ce4fbf3588f8c966ab9548
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-09-17 23:16:15 -0500

    remove all use-strict

--- src/babel.config.js
+++ src/babel.config.js
@@ -1,4 +1,11 @@
-let presets = [require('babel-preset-atomic')];
+let presets = [
+  [
+    'babel-preset-atomic',
+    {
+      removeAllUseStrict: true // some of the packages use non-strict JavaScript in ES6 modules! We should fix those and then make it `false`
+    }
+  ]
+];
 
 let plugins = [];
 
