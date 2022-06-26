3399c06de1c874327da3b6e37f0d87fd0c1101f0
steven nguyen <nguyeste008@students.garlandisd.net>
2022-06-26 03:21:15 +0000

    lint

--- packages/language-sass/spec/scss-spec.js
+++ packages/language-sass/spec/scss-spec.js
@@ -1,19 +1,21 @@
-const dedent = require('dedent')
+const dedent = require('dedent');
 
 describe('Language sass', () => {
   beforeEach(async () => {
-    atom.config.set('core.useTreeSitterParsers', false)
-    await atom.packages.activatePackage('language-sass')
-  })
+    atom.config.set('core.useTreeSitterParsers', false);
+    await atom.packages.activatePackage('language-sass');
+  });
 
   it('Should tokenize - as selector', async () => {
-    const editor = await atom.workspace.open("foo.scss")
+    const editor = await atom.workspace.open('foo.scss');
 
     editor.setText(dedent`
       .foo {
         @extend .foo-bar-baz;
-      }`)
+      }`);
 
-    expect(editor.scopeDescriptorForBufferPosition([1,14]).toString()).toBe('.source.css.scss .meta.property-list.scss .meta.at-rule.extend.scss .entity.other.attribute-name.class.css')
-  })
-})
+    expect(editor.scopeDescriptorForBufferPosition([1, 14]).toString()).toBe(
+      '.source.css.scss .meta.property-list.scss .meta.at-rule.extend.scss .entity.other.attribute-name.class.css'
+    );
+  });
+});
