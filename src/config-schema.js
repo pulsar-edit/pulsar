4f195f8a0a1042f0f2a04f2fdf11bfcb04787d83
confused-Techie <dev@lhbasics.com>
2022-07-23 01:41:40 -0700

    Added @autumnblazey suggestions

--- src/config-schema.js
+++ src/config-schema.js
@@ -7,18 +7,18 @@ const configSchema = {
     properties: {
       name: {
         type: 'string',
-        default: 'pulsar',
-        description: 'Casual name to refer to the Editor.'
-      },
-      properName: {
-        type: 'string',
         default: 'Pulsar',
         description: 'Captilized version of the standard name, to refer to the Editor.'
       },
-      longName: {
+      fullName: {
         type: 'string',
         default: 'Pulsar Edit',
         description: 'Proper Long Name of the Editor.'
+      },
+      id: {
+        type: 'string',
+        default: 'pulsar',
+        description: 'Casual name to refer to the Editor.'
       }
     }
   },
