1c6ce90a9c5959ce44616e693d98448446f07c5a
Maurício Szabo <mauricio.szabo@gmail.com>
2022-08-17 18:40:42 -0300

    Trying to distribute tests

--- script/run-package-tests.js
+++ script/run-package-tests.js
@@ -2,21 +2,26 @@ const cp = require('child_process')
 const fs = require('fs')
 const path = require('path')
 const packJson = require('../package.json')
-let packagePath = []
 
-for(let pack in packJson.packageDependencies) {
-  let basePath = path.join('node_modules', pack)
-  let testPath = path.join(basePath, 'test')
-  let specPath = path.join(basePath, 'spec')
-  if(fs.existsSync(testPath)) packagePath.push(testPath)
-  if(fs.existsSync(specPath)) packagePath.push(specPath)
+module.exports = function(filter) {
+  let packagePath = []
+
+  for(let pack in packJson.packageDependencies) {
+    if(pack.match(filter)) {
+      let basePath = path.join('node_modules', pack)
+      let testPath = path.join(basePath, 'test')
+      let specPath = path.join(basePath, 'spec')
+      if(fs.existsSync(testPath)) packagePath.push(testPath)
+      if(fs.existsSync(specPath)) packagePath.push(specPath)
+    }
+  }
+
+  // console.log('yarn', 'start', '--test', ...packagePath)
+  const res = cp.spawnSync('yarn', ['start', '--test', ...packagePath], {
+    cwd: process.cwd(),
+    detached: true,
+    stdio: "inherit"
+  })
+
+  process.exit(res.status)
 }
-
-// console.log('yarn', 'start', '--test', ...packagePath)
-const res = cp.spawnSync('yarn', ['start', '--test', ...packagePath], {
-  cwd: process.cwd(),
-  detached: true,
-  stdio: "inherit"
-})
-
-process.exit(res.status)
