ef17344c6ebf8775956902ec1dccb027a02cbabe
Maurício Szabo <mauricio.szabo@gmail.com>
2022-08-18 21:00:33 -0300

    Making APM work on dev builds

--- src/package-manager.js
+++ src/package-manager.js
@@ -190,7 +190,7 @@ module.exports = class PackageManager {
   // Return a {String} file path to apm.
   getApmPath() {
     const configPath = atom.config.get('core.apmPath');
-    if (configPath || this.apmPath) {
+    if (process.env.APM_PATH || configPath || this.apmPath) {
       return configPath || this.apmPath;
     }
 
