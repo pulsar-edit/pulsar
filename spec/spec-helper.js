da622e05d9bf2ac70df7c9dac1c9cca6da0a8e90
confused-Techie <dev@lhbasics.com>
2022-09-05 14:28:14 -0700

    Mocked Debounce

--- spec/spec-helper.js
+++ spec/spec-helper.js
@@ -99,6 +99,62 @@ beforeEach(function() {
   spyOn(Date, 'now').andCallFake(() => window.now);
   spyOn(window, "setTimeout").andCallFake(window.fakeSetTimeout);
   spyOn(window, "clearTimeout").andCallFake(window.fakeClearTimeout);
+  spyOn(_, "debounce").andCallFake(function(func, wait, immediate) {
+    var timeout, previous, args, result, context;
+    
+    // Declaring Rest Arguments here, since its included in our debounce function 
+    var restArguments = function(func, startIndex) {
+      startIndex = startIndex == null ? func.length - 1 : +startIndex;
+      return function() {
+        var length = Math.max(arguments.length = startIndex, 0),
+          rest = Array(length),
+          index = 0;
+        for (; index < length; index++) {
+          rest[index] = arguments[index + startIndex];
+        }
+        switch (startIndex) {
+          case 0: return func.call(this, rest);
+          case 1: return func.call(this, arguments[0], rest);
+          case 2: return func.call(this, arguments[0], arguments[1], rest);
+        }
+        var args = Array(startIndex + 1);
+        for (index = 0; index < startIndex; index++) {
+          args[index] = arguments[index];
+        }
+        args[startIndex] = rest;
+        return func.apply(this, args);
+      };
+    };
+    
+    var later = function() {
+      var passed = Date.now() - previous;
+      if (wait > passed) {
+        timeout = setTimeout(later, wait - passed);
+      } else {
+        timeout = null;
+        if (!immediate) result = func.apply(context, args);
+        if (!timeout) args = context = null;
+      }
+    };
+    
+    var debounced = restArguments(function(_args) {
+      context = this;
+      args = _args;
+      previous = Date.now();
+      if (!timeout) {
+        timeout = setTimeout(later, wait);
+        if (immediate) result = func.apply(context, args);
+      }
+      return result;
+    });
+    
+    debounced.cancel = function() {
+      clearTimeout(timeout);
+      timeout = args = context = null;
+    };
+    
+    return debounced;
+  });
 
   const spy = spyOn(atom.packages, 'resolvePackagePath').andCallFake(function(packageName) {
     if (specPackageName && (packageName === specPackageName)) {
