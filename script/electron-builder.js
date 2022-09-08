8615bd6b2da11b1130117c445e3ddba2a5f8eb24
Maurício Szabo <mauricio.szabo@gmail.com>
2022-09-08 15:25:14 -0300

    Trying to exclude paths that we don't need on Pulsar

--- script/electron-builder.js
+++ script/electron-builder.js
@@ -46,6 +46,31 @@ let options = {
   "appId": "link.mauricioszabo.pulsar",
   "npmRebuild": false,
   "publish": null,
+  files: [
+    "package.json",
+    "docs/**/*",
+    "dot-atom/**/*",
+    "exports/**/*",
+    "keymaps/**/*",
+    "menus/**/*",
+    "node_modules/**/*",
+    "resources/**/*",
+    "script/**/*",
+    "src/**/*",
+    "static/**/*",
+    "vendor/**/*",
+    "!**/node_modules/*/{CHANGELOG.md,README.md,README,readme.md,readme}",
+    "!**/node_modules/*/{test,__tests__,tests,powered-test,example,examples}",
+    "!**/node_modules/*.d.ts",
+    "!**/node_modules/.bin",
+    "!**/*.{iml,o,hprof,orig,pyc,pyo,rbc,swp,csproj,sln,xproj}",
+    "!.editorconfig",
+    "!**/._*",
+    "!**/{.DS_Store,.git,.hg,.svn,CVS,RCS,SCCS,.gitignore,.gitattributes}",
+    "!**/{__pycache__,thumbs.db,.flowconfig,.idea,.vs,.nyc_output}",
+    "!**/{appveyor.yml,.travis.yml,circle.yml}",
+    "!**/{npm-debug.log,yarn.lock,.yarn-integrity,.yarn-metadata.json}"
+  ],
   "extraResources": [
     {
       "from": "ppm",
@@ -69,10 +94,10 @@ let options = {
         "target": "deb",
         "arch": "x64"
       },
-      // {
-      //   "target": "rpm",
-      //   "arch": "x64"
-      // }
+      {
+        "target": "rpm",
+        "arch": "x64"
+      }
     ]
   },
   "mac": {
