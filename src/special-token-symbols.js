1ac08568599be449987c1a560634cbb3b80296c5
Sertonix <sert0nix@protonmail.com>
2022-10-17 23:46:32 +0200

    fix wrong decaffeination

--- src/special-token-symbols.js
+++ src/special-token-symbols.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SoftTab: Symbol('SoftTab')
-  HardTab: Symbol('HardTab')
-  PairedCharacter: Symbol('PairedCharacter')
+  SoftTab: Symbol('SoftTab'),
+  HardTab: Symbol('HardTab'),
+  PairedCharacter: Symbol('PairedCharacter'),
   SoftWrapIndent: Symbol('SoftWrapIndent')
 }
