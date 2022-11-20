292e3ddf30d0996da26649f0dc37ea1186380a6f
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-11-20 16:30:19 +0100

    build packages script

---
+++ script/build-packages.js
@@ -0,0 +1,22 @@
+const fs = require('fs/promises');
+const cp = require('child_process')
+
+const packagesPath = path.join(__dirname, '..', 'packages');
+
+(async()=>{
+  const responses = await Promise.all((await fs.readdir(packagesPath)).map( packName =>
+    cp.spawn('npm', ['i'], {
+      cwd: path.join(packagesPath, packName)
+    })
+  ));
+  
+  const fails = responses.filter( res => res.status !== 0 );
+  if (fails.length !== 0) {
+    process.exit(1);
+  } else {
+    process.exit(0);
+  }
+})().catch( e => {
+  console.error(e);
+  process.exit(1);
+});
