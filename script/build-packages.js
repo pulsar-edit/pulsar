6efdb1c4c35e40c5fe17f809d321b30d60f9ac82
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-11-20 16:45:20 +0100

    check if directory

--- script/build-packages.js
+++ script/build-packages.js
@@ -5,11 +5,11 @@ const path = require('path');
 const packagesPath = path.join(__dirname, '..', 'packages');
 
 (async()=>{
-  const responses = await Promise.all((await fs.readdir(packagesPath)).map( packName =>
-    cp.spawn('npm', ['i'], {
-      cwd: path.join(packagesPath, packName)
-    })
-  ));
+  const responses = await Promise.all((await fs.readdir(packagesPath,{withFileTypes:true})).map( dirent =>
+    dirent.isDirectory ? cp.spawn('npm', ['i'], {
+      cwd: path.join(packagesPath, dirent.name)
+    }) : null
+  )).filter( p => p );
   
   const fails = responses.filter( res => res.status !== 0 );
   if (fails.length !== 0) {
