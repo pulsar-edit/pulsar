0eedd8ddfe0204883ded821a9a64a80d078270eb
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-11-20 22:34:00 +0100

    update package build script

--- script/build-packages.js
+++ script/build-packages.js
@@ -6,13 +6,14 @@ const packagesPath = path.join(__dirname, '..', 'packages');
 
 const dirents = fs.readdirSync(packagesPath,{withFileTypes:true});
 
-const [command,...args] = ['npm','install','--omit=dev','--legacy-peer-deps'/* this should be removed if possible */];
+const [command,...args] = ['npm','install','--legacy-peer-deps'/* this should be removed if possible */];
 
 for (const dirent of dirents) {
   if (!dirent.isDirectory()) continue;
-  console.log(`packages/${dirent.name}> ${command} ${args.join(' ')}`);
+  const packagePath = path.join(packagesPath, dirent.name);
+  console.log(`${packagePath}$ ${command} ${args.join(' ')}`);
   const res = cp.spawnSync(command,args,{
-    cwd: path.join(packagesPath, dirent.name),
+    cwd: packagePath,
     shell: true,
     stdio: "inherit",
   });
