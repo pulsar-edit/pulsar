50b3dcdee957f53e2f4af58315d8d4c74aff2426
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-11-20 22:08:10 +0100

    build packages sync

--- script/build-packages.js
+++ script/build-packages.js
@@ -1,19 +1,23 @@
-const fs = require('fs/promises');
+const fs = require('fs');
 const cp = require('child_process');
 const path = require('path');
 
 const packagesPath = path.join(__dirname, '..', 'packages');
 
-fs.readdir(packagesPath,{withFileTypes:true}).then( dirents => Promise.all(dirents.map( dirent => {
-  if (!dirent.isDirectory()) return;
-  return new Promise((resolve, reject) =>
-    cp.exec(
-      `npm i`,
-      { cwd: path.join(packagesPath, dirent.name) },
-      (error, stdout, stderr) => error ? reject(error) : resolve()
-    )
-  );
-}))).catch( e => {
-  console.error(e);
-  process.exit(1);
-});
+const dirents = fs.readdirSync(packagesPath,{withFileTypes:true});
+
+const [command,...args] = ['npm','install','--omit=dev','--legacy-peer-deps'/* this should be removed if possible */];
+
+for (const dirent of dirents) {
+  if (!dirent.isDirectory()) continue;
+  console.log(`packages/${dirent.name}> ${command} ${args.join(' ')}`);
+  const res = cp.spawnSync(command,args,{
+    cwd: path.join(packagesPath, dirent.name),
+    shell: true,
+    stdio: "inherit",
+  });
+  if (res.status) {
+    process.exit(res.status);
+    break;
+  }
+}
