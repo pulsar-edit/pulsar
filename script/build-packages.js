9c6e14f51b49c807baea52cb372d9eb26b2cda14
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-11-20 17:03:32 +0100

    redo package builder

--- script/build-packages.js
+++ script/build-packages.js
@@ -4,20 +4,16 @@ const path = require('path');
 
 const packagesPath = path.join(__dirname, '..', 'packages');
 
-(async()=>{
-  const responses = await Promise.all((await fs.readdir(packagesPath,{withFileTypes:true})).map( dirent =>
-    dirent.isDirectory ? cp.spawn('npm', ['i'], {
-      cwd: path.join(packagesPath, dirent.name)
-    }) : null
-  )).filter( p => p );
-  
-  const fails = responses.filter( res => res.status !== 0 );
-  if (fails.length !== 0) {
-    process.exit(1);
-  } else {
-    process.exit(0);
-  }
-})().catch( e => {
+fs.readdir(packagesPath,{withFileTypes:true}).then( dirents => Promise.all(dirents.map( dirent => {
+  if (!dirent.isDirectory()) return;
+  return new Promise((resolve, reject) =>
+    child.exec(
+      `npm i`,
+      { cwd: path.join(packagesPath, dirent.name) },
+      (error, stdout, stderr) => error ? reject(error) : resolve()
+    )
+  );
+}))).catch( e => {
   console.error(e);
   process.exit(1);
 });
