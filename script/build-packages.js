7f20d4eeb1199df76489f4439b938ff9e0e833d3
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-11-21 16:21:06 +0100

    add build command for packages

--- script/build-packages.js
+++ script/build-packages.js
@@ -3,18 +3,16 @@ const cp = require('child_process');
 const path = require('path');
 
 const packagesPath = path.join(__dirname, '..', 'packages');
+const apmPath = path.join(__dirname, '..', 'ppm', 'bin', process.platform === 'win32' ? 'apm.cmd' : 'apm');
 
 const dirents = fs.readdirSync(packagesPath,{withFileTypes:true});
-
-const [command,...args] = ['npm','install','--legacy-peer-deps'/* this should be removed if possible */];
 
 for (const dirent of dirents) {
   if (!dirent.isDirectory()) continue;
   const packagePath = path.join(packagesPath, dirent.name);
-  console.log(`${packagePath}$ ${command} ${args.join(' ')}`);
-  const res = cp.spawnSync(command,args,{
+  console.log(`${packagePath}$ apm ci`);
+  const res = cp.spawnSync(apmPath,['ci'],{
     cwd: packagePath,
-    shell: true,
     stdio: "inherit",
   });
   if (res.status) {
