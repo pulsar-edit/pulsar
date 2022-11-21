e20334de97687d1cd7706a35cd4eaace44f17673
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-11-21 16:40:42 +0100

    fix build command not running

--- script/build-packages.js
+++ script/build-packages.js
@@ -3,16 +3,16 @@ const cp = require('child_process');
 const path = require('path');
 
 const packagesPath = path.join(__dirname, '..', 'packages');
-const apmPath = path.join(__dirname, '..', 'ppm', 'bin', process.platform === 'win32' ? 'apm.cmd' : 'apm');
 
 const dirents = fs.readdirSync(packagesPath,{withFileTypes:true});
 
 for (const dirent of dirents) {
   if (!dirent.isDirectory()) continue;
   const packagePath = path.join(packagesPath, dirent.name);
-  console.log(`${packagePath}$ apm ci`);
-  const res = cp.spawnSync(apmPath,['ci'],{
+  console.log(`${packagePath}$ npm ci`);
+  const res = cp.spawnSync('npm',['ci','--legacy-peer-deps'],{
     cwd: packagePath,
+    shell: true,
     stdio: "inherit",
   });
   if (res.status) {
