07f888e541a6b2b0b974f9739041ab3050802cec
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-11-25 20:55:10 +0100

    join editor tests

--- script/run-package-tests.js
+++ script/run-package-tests.js
@@ -3,26 +3,24 @@ const fs = require('fs')
 const path = require('path')
 const packJson = require('../package.json')
 
-module.exports = function(filter) {
-  let packagePath = []
+let packagePath = []
 
-  const packagesPath = path.join(__dirname, '..', 'packages')
-  for(let pack of fs.readdirSync(packagesPath)) {
-    if(pack.match(filter)) {
-      let basePath = path.join(packagesPath, pack)
-      let testPath = path.join(basePath, 'test')
-      let specPath = path.join(basePath, 'spec')
-      if(fs.existsSync(testPath)) packagePath.push(testPath)
-      if(fs.existsSync(specPath)) packagePath.push(specPath)
-    }
+const packagesPath = path.join(__dirname, '..', 'packages')
+for(let pack of fs.readdirSync(packagesPath)) {
+  if(pack.match(filter)) {
+    let basePath = path.join(packagesPath, pack)
+    let testPath = path.join(basePath, 'test')
+    let specPath = path.join(basePath, 'spec')
+    if(fs.existsSync(testPath)) packagePath.push(testPath)
+    if(fs.existsSync(specPath)) packagePath.push(specPath)
   }
-
-  // console.log('yarn', 'start', '--test', ...packagePath)
-  const res = cp.spawnSync('yarn', ['start', '--test', ...packagePath], {
-    cwd: process.cwd(),
-    detached: true,
-    stdio: "inherit"
-  })
-
-  process.exit(res.status)
 }
+
+// console.log('yarn', 'start', '--test', ...packagePath)
+const res = cp.spawnSync('yarn', ['start', '--test', ...packagePath], {
+  cwd: process.cwd(),
+  detached: true,
+  stdio: "inherit"
+})
+
+process.exit(res.status)
