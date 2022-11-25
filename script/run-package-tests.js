dff9921acd7c005b3e493b2f596f417e827723fc
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-11-25 21:18:07 +0100

    fix error

--- script/run-package-tests.js
+++ script/run-package-tests.js
@@ -7,13 +7,11 @@ let packagePath = []
 
 const packagesPath = path.join(__dirname, '..', 'packages')
 for(let pack of fs.readdirSync(packagesPath)) {
-  if(pack.match(filter)) {
-    let basePath = path.join(packagesPath, pack)
-    let testPath = path.join(basePath, 'test')
-    let specPath = path.join(basePath, 'spec')
-    if(fs.existsSync(testPath)) packagePath.push(testPath)
-    if(fs.existsSync(specPath)) packagePath.push(specPath)
-  }
+  let basePath = path.join(packagesPath, pack)
+  let testPath = path.join(basePath, 'test')
+  let specPath = path.join(basePath, 'spec')
+  if(fs.existsSync(testPath)) packagePath.push(testPath)
+  if(fs.existsSync(specPath)) packagePath.push(specPath)
 }
 
 // console.log('yarn', 'start', '--test', ...packagePath)
