ccd95fbf202ca301485591034e7c1c643a2f4ee0
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-11-26 19:53:12 +0100

    cleanup package build

--- script/build-packages.js
+++ script/build-packages.js
@@ -2,14 +2,9 @@ const fs = require('fs');
 const cp = require('child_process');
 const path = require('path');
 
-/*
-    env.npm_config_runtime = "electron"
-    env.npm_config_target = @electronVersion
-    env.npm_config_disturl = config.getElectronUrl()
-    env.npm_config_arch = config.getElectronArch()
-    env.npm_config_target_arch = config.getElectronArch() # for node-pre-gyp
-
-*/
+const arch = process.arch;
+const target = "12.2.3"; // targeted electron version
+const disturl = "https://electronjs.org/headers";
 
 const packagesPath = path.join(__dirname, '..', 'packages');
 const dirents = fs.readdirSync(packagesPath,{withFileTypes:true});
@@ -19,28 +14,15 @@ const env = Object.assign({},process.env,{
   npm_config_target_arch: process.arch,
   npm_config_disturl: "https://electronjs.org/headers",
   npm_config_runtime: "electron",
-  // HOME: path.join(process.env.USERPROFILE,"./.electron-gyp"),
 });
-
-/*
-export npm_config_target=1.2.3
-export npm_config_arch=x64
-export npm_config_target_arch=x64
-export npm_config_disturl=https://electronjs.org/headers
-export npm_config_runtime=electron
-export npm_config_build_from_source=true
-HOME=~/.electron-gyp npm install
-*/
 
 for (const dirent of dirents) {
   if (!dirent.isDirectory()) continue;
   const packagePath = path.join(packagesPath, dirent.name);
   if (false) {
-    fs.rmSync(packagePath,{recursive:true});
-    break;
   }
   console.log(`${packagePath}$ npm i`);
-  const res = cp.spawnSync('npm',['i','--legacy-peer-deps','--target=12.2.3','--disturl=https://atom.io/download/electron','--arch=' + process.arch],{
+  const res = cp.spawnSync('npm',['i','--legacy-peer-deps','--target=' + target,'--disturl=' + disturl,'--arch=' + arch],{
     cwd: packagePath,
     env,
     shell: true,
