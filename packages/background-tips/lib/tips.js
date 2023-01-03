1dfaede2c95e10499914be7c326bdf13bebc70b4
confused-Techie <dev@lhbasics.com>
2023-01-03 03:04:09 -0800

    Bundle `background-tips`

---
+++ packages/background-tips/lib/tips.js
@@ -0,0 +1,14 @@
+module.exports = [
+  'Close panels like find and replace with {body>core:cancel}',
+  `Everything ${atom.branding.name} can do is in the Command Palette. See it by using {command-palette:toggle}`,
+  'You can quickly open files with the Fuzzy Finder. Try it by using {fuzzy-finder:toggle-file-finder}',
+  'You can toggle the Tree View with {tree-view:toggle}',
+  'You can focus the Tree View with {tree-view:toggle-focus}',
+  'You can toggle the Git tab with {github:toggle-git-tab}',
+  'You can focus the Git tab with {github:toggle-git-tab-focus}',
+  'You can toggle the GitHub tab with {github:toggle-github-tab}',
+  'You can focus the GitHub tab with {github:toggle-github-tab-focus}',
+  'You can split a pane with {pane:split-right-and-copy-active-item}',
+  'You can jump to a method in the editor using {symbols-view:toggle-file-symbols}',
+  'You can install packages and themes from the Settings View {settings-view:open}'
+]
