b327ae17874790bfa44bf41e793f5314590a9160
Sertonix <83883937+Sertonix@users.noreply.github.com>
2023-01-05 18:43:00 +0100

    remove fs-plus from image-view package

--- packages/image-view/spec/image-editor-status-view-spec.js
+++ packages/image-view/spec/image-editor-status-view-spec.js
@@ -1,6 +1,6 @@
 const {it, fit, ffit, beforeEach, afterEach, conditionPromise, emitterEventPromise} = require('./async-spec-helpers') // eslint-disable-line no-unused-vars
 
-const fs = require('fs-plus')
+const fs = require('fs')
 const path = require('path')
 
 describe('ImageEditorStatusView', () => {
