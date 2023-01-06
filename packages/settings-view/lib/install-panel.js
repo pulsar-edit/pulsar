88c9845c981f3bd85014dc9892b4a4a8a4e27ec2
Sertonix <83883937+Sertonix@users.noreply.github.com>
2023-01-06 11:13:04 +0000

    include debounce function

--- packages/settings-view/lib/install-panel.js
+++ packages/settings-view/lib/install-panel.js
@@ -5,7 +5,6 @@ import path from 'path'
 import electron from 'electron'
 import etch from 'etch'
 import hostedGitInfo from 'hosted-git-info'
-import {debounce} from 'text-buffer/lib/helpers'
 
 import {CompositeDisposable, TextEditor} from 'atom'
 
@@ -46,7 +45,10 @@ export default class InstallPanel {
       })
     )
     const searchBuffer = this.refs.searchEditor.getBuffer();
-    searchBuffer.debouncedEmitDidStopChangingEvent = debounce(searchBuffer.emitDidStopChangingEvent.bind(searchBuffer), 700);
+    searchBuffer.debouncedEmitDidStopChangingEvent = (timer => () => {
+      clearTimeout(timer);
+      timer = setTimeout(searchBuffer.emitDidStopChangingEvent.bind(searchBuffer), 700);
+    })();
     // TODO remove hack to extend stop changing delay
     this.disposables.add(
       this.refs.searchEditor.onDidStopChanging(() => {
