13da05fd4af188ed5edda73b52a6f251df8a1a66
confused-Techie <dev@lhbasics.com>
2023-01-27 20:25:35 -0800

    Remove Decaf Comment

--- packages/tabs/lib/tab-bar-view.js
+++ packages/tabs/lib/tab-bar-view.js
@@ -1,13 +1,3 @@
-/*
- * decaffeinate suggestions:
- * DS101: Remove unnecessary use of Array.from
- * DS102: Remove unnecessary code created because of implicit returns
- * DS104: Avoid inline assignments
- * DS205: Consider reworking code to avoid use of IIFEs
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
-
 let BrowserWindow = null; // Defer require until actually used
 const {ipcRenderer} = require('electron');
 
