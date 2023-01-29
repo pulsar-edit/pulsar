adb77b1f366b23bb2d768d1727c270cb07fa5de9
confused-Techie <dev@lhbasics.com>
2023-01-28 22:04:51 -0800

    Removed decaf comments

--- packages/tabs/spec/tabs-spec.js
+++ packages/tabs/spec/tabs-spec.js
@@ -1,10 +1,3 @@
-/*
- * decaffeinate suggestions:
- * DS101: Remove unnecessary use of Array.from
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
 const _ = require('underscore-plus');
 const path = require('path');
 const temp = require('temp');
