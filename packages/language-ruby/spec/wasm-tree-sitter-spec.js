03d120bd68e65759ae1ca91c4cd9195652826b61
Maurício Szabo <mauricio.szabo@gmail.com>
2023-02-24 21:09:22 -0300

    Refactored test data

--- packages/language-ruby/spec/wasm-tree-sitter-spec.js
+++ packages/language-ruby/spec/wasm-tree-sitter-spec.js
@@ -12,48 +12,15 @@ describe('WASM Tree-sitter Ruby grammar', () => {
   it('tokenizes symbols', async () => {
     const editor = await openDocument('classes-wasm-ts.rb');
 
-    let allMatches = [], lastNonComment = 0
-    editor.getBuffer().getLines().forEach((row, i) => {
-      const m = row.match(/#/)
-      if(m) {
-        const scope = editor.scopeDescriptorForBufferPosition([i, m.index])
-        if(scope.scopes.find(s => s.match(/comment/))) {
-          allMatches.push({row: lastNonComment, text: row, col: m.index})
-          return
-        }
-      }
-      lastNonComment = i
+    normalizeTestData(editor, /#/).forEach(({expected, editorPosition, testPosition}) => {
+      expect(editor.scopeDescriptorForBufferPosition(editorPosition).scopes).toSatisfy((scopes, reason) => {
+        reason(dedent`
+          Expected to find scope "${expected}" but found "${scopes}"
+          at class-wasm-ts.rb:${testPosition.row+1}:${testPosition.column+1}
+        `)
+        return scopes.indexOf(expected) !== -1
+      })
     })
-    expect(allMatches).toSatisfy((matches, reason) => {
-      reason("Tokenizer wasn't able to run")
-      return matches.length > 0
-    })
-
-    allMatches.forEach(({text, row, col}) => {
-      const exactPos = text.match(/\^\s+(.*)/)
-      if(exactPos) {
-        expect(editor.scopeDescriptorForBufferPosition([row, exactPos.index]).scopes).toSatisfy((scopes, reason) => {
-          const expected = exactPos[1]
-          reason(dedent`
-            Expected to find scope "${expected}" but found "${scopes}"
-            at class-wasm-ts.rb:${row+1}:${exactPos.index+1}
-          `)
-          return scopes.indexOf(expected) !== -1
-        })
-      } else {
-        const pos = text.match(/\<-\s+(.*)/)
-        // console.log('Finding Scope', pos[1], 'on', [row, col], 'and scopes:', editor.scopeDescriptorForBufferPosition([row, col]))
-        expect(editor.scopeDescriptorForBufferPosition([row, col]).scopes).toSatisfy((scopes, reason) => {
-          const expected = pos[1]
-          reason(dedent`
-            Expected to find scope "${expected}" but found "${scopes}"
-            at class-wasm-ts.rb:${row+1}:${col+1}
-          `)
-          return scopes.indexOf(expected) !== -1
-        })
-      }
-    })
-    const mode = editor.languageMode
   });
 });
 
@@ -61,10 +28,38 @@ async function openDocument(fileName) {
   const fullPath = path.join(__dirname, 'fixtures', fileName)
   const editor = await atom.workspace.open(fullPath)
   await editor.languageMode.ready
-  // editor.languageMode.buildHighlightIterator().seek({row: 0, column: 0} )
-  // await new Promise(resolve => {
-  //   console.log("WAT")
-  //   editor.languageMode.onDidTokenize(resolve)
-  // })
   return editor
+}
+
+function normalizeTestData(editor, commentRegex) {
+  let allMatches = [], lastNonComment = 0
+  editor.getBuffer().getLines().forEach((row, i) => {
+    const m = row.match(commentRegex)
+    if(m) {
+      const scope = editor.scopeDescriptorForBufferPosition([i, m.index])
+      if(scope.scopes.find(s => s.match(/comment/))) {
+        allMatches.push({row: lastNonComment, text: row, col: m.index, testRow: i})
+        return
+      }
+    }
+    lastNonComment = i
+  })
+  return allMatches.map(({text, row, col, testRow}) => {
+    const exactPos = text.match(/\^\s+(.*)/)
+    if(exactPos) {
+      const expected = exactPos[1]
+      return {
+        expected,
+        editorPosition: {row, column: exactPos.index},
+        testPosition: {row: testRow, column: col}
+      }
+    } else {
+      const pos = text.match(/\<-\s+(.*)/)
+      return {
+        expected: pos[1],
+        editorPosition: {row, column: col},
+        testPosition: {row: testRow, column: col}
+      }
+    }
+  })
 }
