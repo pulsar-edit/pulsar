0863b759c045e2ea6d18794bc0ae91ba21057859
Maurício Szabo <mauricio.szabo@gmail.com>
2023-03-06 17:39:14 -0300

    Removing VERY specific matches

--- packages/language-ruby/spec/fixtures/textmate-grammar.rb
+++ packages/language-ruby/spec/fixtures/textmate-grammar.rb
@@ -1,165 +1,167 @@
 require "."
-# <- keyword.other.special-method.ruby
+# <- keyword.other.special-method
 
 class A::B < C
-# <- keyword.control.class.ruby
-#     ^ entity.name.type.class.ruby
-#      ^ punctuation.separator.namespace.ruby
-#        ^ entity.name.type.class.ruby
-#          ^ punctuation.separator.inheritance.ruby
-#            ^ entity.other.inherited-class.ruby
-#            ^ entity.name.type.class.ruby
+# <- keyword.control.class
+#     ^ entity.name.type.class
+#      ^ punctuation.separator.namespace
+#        ^ entity.name.type.class
+#          ^ punctuation.separator.inheritance
+#            ^ entity.other.inherited-class
+#            ^ entity.name.type.class
   public
-  # <- keyword.other.special-method.ruby
+  # <- keyword.other.special-method
   protected
-  # <- keyword.other.special-method.ruby
+  # <- keyword.other.special-method
   private
-  # <- keyword.other.special-method.ruby
+  # <- keyword.other.special-method
 
   public def foo(a)
-  # <- keyword.other.special-method.ruby
-  #      ^ keyword.control.def.ruby
-  #          ^ entity.name.function.ruby
-  #              ^ variable.parameter.function.ruby
+  # <- keyword.other.special-method
+  #      ^ keyword.control.def
+  #          ^ entity.name.function
+  #              ^ variable.parameter.function
     self
-    # <- variable.language.self.ruby
+    # <- variable.language.self
     @b = a
-    # <- variable.other.readwrite.instance.ruby
-    #  ^ keyword.operator.assignment.ruby
-    #    ^ source.ruby
+    # <- variable.other.readwrite.instance
+    #  ^ keyword.operator.assignment
+    #    ^ source
     c = 10
-    # <- source.ruby
+    # <- source
   end
 
 end
 
 thing&.call
-#    ^ punctuation.separator.method.ruby
-#      ^ source.ruby
+#    ^ punctuation.separator.method
+#      ^ source
 
 VAR1 = 100
-# <- variable.other.constant.ruby
-#    ^ keyword.operator.assignment.ruby
-#      ^ constant.numeric.ruby
+# <- variable.other.constant
+#    ^ keyword.operator.assignment
+#      ^ constant.numeric
 
 _VAR1 = 100_000
-# <- variable.other.constant.ruby
-#       ^ constant.numeric.ruby
+# <- variable.other.constant
+#       ^ constant.numeric
 
 # This dot will not be tokenized as a separator
 1.23
-#^ constant.numeric.ruby
+#^ constant.numeric
 
 # But this will
 a.b
-#^ punctuation.separator.method.ruby
+#^ punctuation.separator.method
 
 # These are all also numbers:
 1.23e-4
-# <- constant.numeric.ruby
+# <- constant.numeric
 0d100_000
-# <- constant.numeric.ruby
+# <- constant.numeric
 0xAFFF
-# <- constant.numeric.ruby
+# <- constant.numeric
 0XA_FFF
-# <- constant.numeric.ruby
+# <- constant.numeric
 01_777
-# <- constant.numeric.ruby
+# <- constant.numeric
 0o1_777
-# <- constant.numeric.ruby
+# <- constant.numeric
 0b100_000
-# <- constant.numeric.ruby
+# <- constant.numeric
 0B00100
-# <- constant.numeric.ruby
+# <- constant.numeric
 
 :test
-# <- constant.other.symbol.ruby
+# <- constant.other.symbol
 :$symbol
-# <- constant.other.symbol.ruby
+# <- constant.other.symbol
 :<=>
-# <- constant.other.symbol.ruby
+# <- constant.other.symbol
 %i(foo)
-# <- punctuation.section.array.begin.ruby
-#  ^ constant.other.symbol.ruby
+# <- punctuation.section.array.begin
+#  ^ constant.other.symbol
 {foo: 1}
-# <- punctuation.section.scope.begin.ruby
-# ^ constant.other.symbol.hashkey.ruby
+# <- punctuation.section.scope.begin
+# ^ constant.other.symbol.hashkey
+#   ^ punctuation.definition.constant.hashkey
 {:foo => 1}
-# ^ constant.other.symbol.hashkey.ruby
+# ^ constant.other.symbol.hashkey
+#     ^ punctuation.separator.key-value
 
 class << A::B
-# <- keyword.control.class.ruby
-#     ^ punctuation.definition.variable.ruby
-#        ^ entity.name.type.class.ruby
-#         ^ punctuation.separator.namespace.ruby
+# <- keyword.control.class
+#     ^ punctuation.definition.variable
+#        ^ entity.name.type.class
+#         ^ punctuation.separator.namespace
 end
 
 def a.b(*args)
-# <- meta.function.method.with-arguments.ruby
-# <- keyword.control.def.ruby
-#   ^ entity.name.function.ruby
-#    ^ entity.name.function.ruby
-#    ^ punctuation.separator.method.ruby
-#      ^ meta.function.method.with-arguments.ruby
-#      ^ punctuation.definition.parameters.ruby
-#       ^ storage.type.variable.ruby
-#        ^ variable.parameter.function.ruby
-#            ^ punctuation.definition.parameters.ruby
+# <- meta.function.method.with-arguments
+# <- keyword.control.def
+#   ^ entity.name.function
+#    ^ entity.name.function
+#    ^ punctuation.separator.method
+#      ^ meta.function.method.with-arguments
+#      ^ punctuation.definition.parameters
+#       ^ storage.type.variable
+#        ^ variable.parameter.function
+#            ^ punctuation.definition.parameters
 end
-# <- keyword.control.ruby
+# <- keyword.control
 
 # Strings.
 %(te(s)t)
-# <- punctuation.definition.string.begin.ruby
-# ^ string.quoted.other.interpolated.ruby
-#   ^ punctuation.section.scope.ruby
-#    ^ string.quoted.other.interpolated.ruby
-#       ^ punctuation.definition.string.end.ruby
+# <- punctuation.definition.string.begin
+# ^ string.quoted.other.interpolated
+#   ^ punctuation.section.scope
+#    ^ string.quoted.other.interpolated
+#       ^ punctuation.definition.string.end
 
 %[te[s]t]
-# <- punctuation.definition.string.begin.ruby
-# ^ string.quoted.other.interpolated.ruby
-#   ^ punctuation.section.scope.ruby
-#    ^ string.quoted.other.interpolated.ruby
-#       ^ punctuation.definition.string.end.ruby
+# <- punctuation.definition.string.begin
+# ^ string.quoted.other.interpolated
+#   ^ punctuation.section.scope
+#    ^ string.quoted.other.interpolated
+#       ^ punctuation.definition.string.end
 
 %{te{s}t}
-# <- punctuation.definition.string.begin.ruby
-# ^ string.quoted.other.interpolated.ruby
-#   ^ punctuation.section.scope.ruby
-#    ^ string.quoted.other.interpolated.ruby
-#       ^ punctuation.definition.string.end.ruby
+# <- punctuation.definition.string.begin
+# ^ string.quoted.other.interpolated
+#   ^ punctuation.section.scope
+#    ^ string.quoted.other.interpolated
+#       ^ punctuation.definition.string.end
 
 %<te<s>t>
-# <- punctuation.definition.string.begin.ruby
-# ^ string.quoted.other.interpolated.ruby
-#   ^ punctuation.section.scope.ruby
-#    ^ string.quoted.other.interpolated.ruby
-#       ^ punctuation.definition.string.end.ruby
+# <- punctuation.definition.string.begin
+# ^ string.quoted.other.interpolated
+#   ^ punctuation.section.scope
+#    ^ string.quoted.other.interpolated
+#       ^ punctuation.definition.string.end
 
 %~te\~s~
-# <- punctuation.definition.string.begin.ruby
-# ^ string.quoted.other.interpolated.ruby
-#   ^ constant.character.escape.ruby
-#    ^ constant.character.escape.ruby
-#     ^ string.quoted.other.interpolated.ruby
+# <- punctuation.definition.string.begin
+# ^ string.quoted.other.interpolated
+#   ^ constant.character.escape
+#    ^ constant.character.escape
+#     ^ string.quoted.other.interpolated
 
 %Q(te(s)t)
-# <- punctuation.definition.string.begin.ruby
-# ^ string.quoted.other.interpolated.ruby
-#    ^ punctuation.section.scope.ruby
-#     ^ string.quoted.other.interpolated.ruby
-#        ^ punctuation.definition.string.end.ruby
+# <- punctuation.definition.string.begin
+# ^ string.quoted.other.interpolated
+#    ^ punctuation.section.scope
+#     ^ string.quoted.other.interpolated
+#        ^ punctuation.definition.string.end
 
 %x!#{"l" + "s"}!
-# <- punctuation.definition.string.begin.ruby
-# ^ punctuation.definition.string.begin.ruby
-#  ^ punctuation.section.embedded.begin.ruby
-#   ^ punctuation.section.embedded.begin.ruby
-#             ^ punctuation.section.embedded.end.ruby
-#              ^ punctuation.definition.string.end.ruby
+# <- punctuation.definition.string.begin
+# ^ punctuation.definition.string.begin
+#  ^ punctuation.section.embedded.begin
+#   ^ punctuation.section.embedded.begin
+#             ^ punctuation.section.embedded.end
+#              ^ punctuation.definition.string.end
 
 /test/
-# <- punctuation.section.regexp.ruby
-# ^ string.regexp.interpolated.ruby
-#    ^ punctuation.section.regexp.ruby
+# <- punctuation.section.regexp
+# ^ string.regexp.interpolated
+#    ^ punctuation.section.regexp
