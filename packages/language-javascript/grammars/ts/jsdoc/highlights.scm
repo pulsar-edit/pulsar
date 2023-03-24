571e4afa434274d89b12473f52a613e4a14db748
Andrew Dupont <github@andrewdupont.net>
2023-03-23 22:37:05 -0700

    Clean up JSDoc highlighting

--- packages/language-javascript/grammars/ts/jsdoc/highlights.scm
+++ packages/language-javascript/grammars/ts/jsdoc/highlights.scm
@@ -2,10 +2,10 @@
 (document) @comment.block.documentation.js.jsx
 (tag_name) @storage.type.class.jsdoc
 
-; TODO: Join these three with adjustments
-(type) @entity.name.type.instance.jsdoc
-(tag "{" @entity.name.type.instance.jsdoc)
-(tag "}" @entity.name.type.instance.jsdoc)
+((tag (type)) @entity.name.type.instance.jsdoc
+  ; Join the type with its surrounding braces.
+  (#set! startAt firstChild.nextSibling.startPosition)
+  (#set! endAt firstChild.nextSibling.nextSibling.nextSibling.endPosition))
 
 "{" @punctuation.definition.bracket.curly.begin.jsdoc
 "}" @punctuation.definition.bracket.curly.end.jsdoc
