9377d3d8940b7af8410fd8172fa7ee75646fd17e
Andrew Dupont <github@andrewdupont.net>
2023-04-04 00:38:15 -0700

    Organize JavaScript `indents.scm`…
    …and add a couple more smart-indent scenarios.

--- packages/language-javascript/grammars/ts/indents.scm
+++ packages/language-javascript/grammars/ts/indents.scm
@@ -1,9 +1,17 @@
 
+; STATEMENT BLOCKS
+; ================
+
 ; More accurate indentation matching for all blocks delimited by braces.
 (statement_block "}" @match
   (#set! matchIndentOf parent.firstChild.startPosition))
 
-; The closing brace of a switch statement's body should match the indentation of the line where the switch statement starts.
+
+; SWITCH STATEMENTS
+; =================
+
+; The closing brace of a switch statement's body should match the indentation
+; of the line where the switch statement starts.
 (switch_statement
   body: (switch_body "}" @match
     (#set! onlyIfLast true))
@@ -15,12 +23,20 @@
   (#set! matchIndentOf parent.parent.startPosition)
   (#set! offsetIndent 1))
 
+
+; ONE-LINE CONDITIONALS
+; =====================
+
 ; An `if` statement without an opening brace should indent the next line…
 (if_statement
   condition: (parenthesized_expression ")" @indent
   (#set! onlyIfLastTextOnRow true)))
 
-; …and keep that indent level if the user types a comment before the consequence…
+("else" @indent
+  (#set! onlyIfLastTextOnRow true))
+
+; …and keep that indent level if the user types a comment before the
+; consequence…
 (if_statement
   consequence: (empty_statement) @match
   (#set! onlyIfNotStartsOnSameRowAs parent.startPosition)
@@ -53,8 +69,24 @@
     (debugger_statement)
   ] @dedent.next)
 
+(else_clause
+  [
+    (expression_statement)
+    (return_statement)
+    (continue_statement)
+    (break_statement)
+    (throw_statement)
+    (debugger_statement)
+  ] @dedent.next
+  (#set! onlyIfNotStartsOnSameRowAs parent.startPosition))
+
+
+; HANGING INDENT ON SPLIT LINES
+; =============================
+
 ; Any of these at the end of a line indicate the next line should be indented…
-(["||" "&&"] @indent (#set! onlyIfLastTextOnRow true))
+(["||" "&&" "?"] @indent
+  (#set! onlyIfLastTextOnRow true))
 
 ; …and the line after that should be dedented.
 (binary_expression
@@ -62,9 +94,21 @@
     right: (_) @dedent.next
     (#set! onlyIfNotStartsOnSameRowAs parent.startPosition))
 
+; let foo = this.longTernaryCondition() ?
+;   consequenceWhichIsItselfRatherLong :
+;   alternativeThatIsNotBrief;
+;
+; …followed by a dedent.
+(ternary_expression
+  alternative: (_) @dedent.next
+  (#set! onlyIfNotStartsOnSameRowAs parent.startPosition))
 
-(template_substitution "}" @_IGNORE_
-  (#set! final true))
+
+; GENERAL
+; =======
+
+; Weed out `}`s that should not signal dedents.
+(template_substitution "}" @_IGNORE_ (#set! final true))
 
 [
   "{"
@@ -78,8 +122,8 @@
   "]"
 ] @dedent
 
-
 ["case" "default"] @indent
+
 
 ; JSX
 ; ===
