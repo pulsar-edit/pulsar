d8444b4cbebda9ae1ef72f64894d6dc8f06f4f60
Andrew Dupont <github@andrewdupont.net>
2023-04-30 14:07:19 -0700

    Add some highlighting fixes to EJS…
    …the same ones we applied to ERB earlier.

--- packages/language-html/grammars/tree-sitter-embedded-template/ejs/highlights.scm
+++ packages/language-html/grammars/tree-sitter-embedded-template/ejs/highlights.scm
@@ -1,10 +1,30 @@
+; We compare these to `firstChild.endPosition` because directives have a habit
+; of consuming all their preceding whitespace, including newlines.
+([(comment_directive) (directive) (output_directive)] @meta.embedded.block.ejs
+  (#set! adjust.startBeforeFirstMatchOf "\\S")
+  (#set! test.onlyIfNotEndsOnSameRowAs firstChild.endPosition))
 
-(comment_directive) @comment.block
+  ([(comment_directive) (directive) (output_directive)] @meta.embedded.line.ejs
+    (#set! adjust.startBeforeFirstMatchOf "\\S")
+    (#set! test.onlyIfEndsOnSameRowAs firstChild.endPosition))
+
+(comment_directive) @comment.block.ejs
+
+; (output_directive
+;   ["<%=" "%>"] @keyword.control.directive.output)
+;
+(directive
+  ["<%" "<%-" "<%_"] @keyword.directive.begin.ejs
+  (#set! adjust.startBeforeFirstMatchOf "\\S"))
+
+(directive
+  ["_%>" "%>" "-%>"] @keyword.directive.end.ejs)
+
+; BUG: Some `directive` nodes are actually represented as `output_directive`
+; nodes.
+(output_directive
+  ["<%=" "<%" "<%-" "<%_" "<%%"] @keyword.directive.begin.ejs
+  (#set! adjust.startBeforeFirstMatchOf "\\S"))
 
 (output_directive
-  ["<%=" "%>"] @keyword.control.directive.output)
-
-((directive
-  ["<%" "<%_"] @keyword.control.directive
-  ["_%>" "%>"] @keyword.control.directive)
-  (#set! test.final true))
+  ["%>" "-%>" "_%>"] @keyword.directive.end.ejs)
