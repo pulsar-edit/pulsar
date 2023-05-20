49bbc72b1ec63f4bddabfe5072cfae035e32017b
Maurício Szabo <mauricio.szabo@gmail.com>
2023-05-20 00:09:07 -0300

    Adding a custom reporter

---
+++ src/mocha-test-runner/reporter.js
@@ -0,0 +1,58 @@
+
+class Reporter {
+  static setMocha(Mocha) {
+    this.constants = Mocha.Runner.constants;
+  }
+
+  constructor(runner) {
+    this._indents = 0;
+    const stats = runner.stats;
+
+    const {
+      EVENT_RUN_BEGIN,
+      EVENT_RUN_END,
+      EVENT_TEST_FAIL,
+      EVENT_TEST_PASS,
+      EVENT_SUITE_BEGIN,
+      EVENT_SUITE_END
+    } = Reporter.constants;
+
+    runner
+      .once(EVENT_RUN_BEGIN, () => {
+        console.log('start');
+      })
+      .on(EVENT_SUITE_BEGIN, () => {
+        this.increaseIndent();
+      })
+      .on(EVENT_SUITE_END, () => {
+        this.decreaseIndent();
+      })
+      .on(EVENT_TEST_PASS, test => {
+        // Test#fullTitle() returns the suite name(s)
+        // prepended to the test title
+        console.log(`${this.indent()}pass: ${test.fullTitle()}`);
+      })
+      .on(EVENT_TEST_FAIL, (test, err) => {
+        console.log(
+          `${this.indent()}fail: ${test.fullTitle()} - error: ${err.message}`
+        );
+      })
+      .once(EVENT_RUN_END, () => {
+        console.log(`end: ${stats.passes}/${stats.passes + stats.failures} ok`);
+      });
+  }
+
+  indent() {
+    return Array(this._indents).join('  ');
+  }
+
+  increaseIndent() {
+    this._indents++;
+  }
+
+  decreaseIndent() {
+    this._indents--;
+  }
+}
+
+module.exports = Reporter;
