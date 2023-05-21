9a00dfabf1f97b4b1836f8faf76a1f91dabfb7ab
confused-Techie <dev@lhbasics.com>
2023-05-20 17:00:07 -0700

    Simply diff, with extraneous tab

--- packages/settings-view/lib/settings-view.js
+++ packages/settings-view/lib/settings-view.js
@@ -124,7 +124,7 @@ export default class SettingsView {
     if (atom.config.get("settings-view.enableSettingsSearch")) {
       this.addCorePanel('Search', 'search', () => new SearchSettingsPanel(this))
     }
-
+    
     this.addCorePanel('Core', 'settings', () => new GeneralPanel())
     this.addCorePanel('Editor', 'code', () => new EditorPanel())
     if (atom.config.getSchema('core.uriHandlerRegistration').type !== 'any') {
