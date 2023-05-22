05c6de0fed1e49a2b6786a7da81e3b0cdd50bea1
Maurício Szabo <mauricio.szabo@gmail.com>
2023-05-21 21:10:51 -0300

    Huge improvement on Clojure highlighting

--- packages/language-clojure/grammars/ts/highlights.scm
+++ packages/language-clojure/grammars/ts/highlights.scm
@@ -1,58 +1,106 @@
 (quoting_lit
   value: (list_lit
-          "(" @punctuation.section.expression.begin
+          "(" @punctuation.section.expression.begin (#set! test.onlyIfNotDescendantOfNodeWithData "clojure.dismissTag")
           .
           (sym_lit) @meta.symbol (#set! final "true")))
 
 ;; Collections
 (anon_fn_lit
- "(" @punctuation.section.expression.begin
+ "(" @punctuation.section.expression.begin (#set! test.onlyIfNotDescendantOfNodeWithData "clojure.dismissTag")
  .
  (sym_lit) @entity.name.function @meta.expression
  ")" @punctuation.section.expression.end)
 
 (list_lit
- "(" @punctuation.section.expression.begin
+ "(" @punctuation.section.expression.begin (#set! test.onlyIfNotDescendantOfNodeWithData "clojure.dismissTag")
  .
  (sym_lit) @entity.name.function @meta.expression
  ")" @punctuation.section.expression.end)
 
 (vec_lit
- "[" @punctuation.section.vector.begin
- "]" @punctuation.section.vector.end) @meta.vector
+ "[" @punctuation.section.vector.begin (#set! test.onlyIfNotDescendantOfNodeWithData "clojure.dismissTag")
+ "]" @punctuation.section.vector.end)
+@meta.vector
 
 (map_lit
- "{" @punctuation.section.map.begin
- "}" @punctuation.section.map.end) @meta.map
+ "{" @punctuation.section.map.begin (#set! test.onlyIfNotDescendantOfNodeWithData "clojure.dismissTag")
+ "}" @punctuation.section.map.end)
+@meta.map
 
 (set_lit
- ("#" "{") @punctuation.section.set.begin
- "}" @punctuation.section.set.end) @meta.map
+ ("#" "{") @punctuation.section.set.begin (#set! test.onlyIfNotDescendantOfNodeWithData "clojure.dismissTag")
+ "}" @punctuation.section.set.end)
+@meta.map
 
 ; Includes
-((sym_name) @meta.symbol (#eq? @meta.symbol "import")) @keyword.control
-((sym_name) @meta.symbol (#eq? @meta.symbol "require")) @keyword.control
-((sym_name) @meta.symbol (#eq? @meta.symbol "use")) @keyword.control
+((sym_name) @meta.symbol (#eq? @meta.symbol "import") (#set! test.onlyIfNotDescendantOfNodeWithData "clojure.dismissTag")) @keyword.control
+((sym_name) @meta.symbol (#eq? @meta.symbol "require") (#set! test.onlyIfNotDescendantOfNodeWithData "clojure.dismissTag")) @keyword.control
+((sym_name)
+ @meta.symbol
+ (#eq? @meta.symbol "use")
+ (#set! test.onlyIfConfig language-clojure.markDeprecations)
+ (#set! test.onlyIfNotDescendantOfNodeWithData clojure.dismissTag))
+@invalid.deprecated
+
+((sym_name)
+ @meta.symbol
+ (#eq? @meta.symbol "use")
+ (#set! test.onlyIfNotConfig language-clojure.markDeprecations)
+ (#set! test.onlyIfNotDescendantOfNodeWithData clojure.dismissTag))
+@keyword.control
+
+((list_lit
+  "(" @punctuation.section.expression.begin (#set! test.onlyIfNotDescendantOfNodeWithData "clojure.dismissTag")
+  .
+  (sym_lit) @meta.definition.global @keyword.control (#eq? @meta.definition.global "ns")
+  .
+  (sym_lit) @meta.definition.global @entity.global
+  ")" @punctuation.section.expression.end)
+ @meta.namespace.clojure
+ (#set! isNamespace true))
 
 (list_lit
- "(" @punctuation.section.expression.begin
+  "("
+  .
+  (kwd_lit) @invalid.deprecated (#eq? @invalid.deprecated ":use")
+  (#set! test.onlyIfDescendantOfNodeWithData isNamespace)
+  (#set! test.onlyIfConfig language-clojure.markDeprecations))
+
+(list_lit
+ "(" @punctuation.section.expression.begin (#set! test.onlyIfNotDescendantOfNodeWithData "clojure.dismissTag")
  .
- ((sym_lit) @meta.symbol (#eq? @meta.symbol "ns")) @keyword.control @meta.definition.global
+ (sym_lit) @keyword.control (#match? @keyword.control "^def")
  .
  (sym_lit) @meta.definition.global @entity.global
  ")" @punctuation.section.expression.end)
 
-(list_lit
- "(" @punctuation.section.expression.begin
- .
- ((sym_lit) @meta.symbol (#match? @meta.symbol "^def")) @keyword.control
- .
- (sym_lit) @meta.definition.global @entity.global
- ")" @punctuation.section.expression.end)
-
-(sym_lit) @meta.symbol
-(kwd_lit) @constant.keyword
-(str_lit) @string.quoted.double
-(num_lit) @constant.numeric
+((sym_lit) @meta.symbol (#set! test.onlyIfNotDescendantOfNodeWithData "clojure.dismissTag"))
+((kwd_lit) @constant.keyword (#set! test.onlyIfNotDescendantOfNodeWithData "clojure.dismissTag"))
+((str_lit) @string.quoted.double (#set! test.onlyIfNotDescendantOfNodeWithData "clojure.dismissTag"))
+((num_lit) @constant.numeric (#set! test.onlyIfNotDescendantOfNodeWithData "clojure.dismissTag"))
+((nil_lit) @constant.numeric (#set! test.onlyIfNotDescendantOfNodeWithData "clojure.dismissTag"))
+((bool_lit) @constant.language (#set! test.onlyIfNotDescendantOfNodeWithData clojure.dismissTag))
 (comment) @comment.line.semicolon
-(dis_expr) @comment.block.clojure
+((dis_expr)
+ @comment.block.clojure
+ (#set! test.onlyIfConfig language-clojure.dismissTag)
+ (#set! clojure.dismissTag true)
+ (#set! test.final true))
+
+((list_lit
+  "(" @punctuation.section.expression.begin
+  .
+  (sym_lit) @meta.definition.global @keyword.control (#eq? @keyword.control "comment")
+  ")" @punctuation.section.expression.end)
+ @comment.block.clojure
+ (#set! test.onlyIfConfig language-clojure.commentTag)
+ (#set! clojure.dismissTag true))
+
+(list_lit
+ "(" @punctuation.section.expression.begin
+ .
+ (sym_lit) @keyword.control (#eq? @keyword.control "comment")
+ (#set! test.onlyIfNotConfig language-clojure.commentTag)
+ ")" @punctuation.section.expression.end)
+
+("ERROR" @invalid.illegal)
