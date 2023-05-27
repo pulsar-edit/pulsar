fe9d724284feafbdc8518b27ae00f04c2fe1dc70
Maurício Szabo <mauricio.szabo@gmail.com>
2023-05-27 00:28:47 -0300

    Added "atom.api" service

--- src/module-utils.js
+++ src/module-utils.js
@@ -1,6 +1,7 @@
-const getWindowLoadSettings = require('./get-window-load-settings');
 const Module = require('module');
 const path = require('path');
+const getWindowLoadSettings = require('./get-window-load-settings');
+const { apis, apiConsumer } = require('./services/pulsar-apis');
 
 // a require function with both ES5 and ES6 default export support
 function requireModule(path) {
@@ -22,13 +23,26 @@ function requireModule(path) {
   }
 }
 
-function addAtomExport() {
+function addAtomExport(globalAtom) {
   const exportsPath = path.join(
     getWindowLoadSettings().resourcePath,
     'exports'
   );
   Module.globalPaths.push(exportsPath);
   process.env.NODE_PATH = exportsPath; // Set NODE_PATH env variable since tasks may need it.
+  patchRequire(globalAtom);
+}
+
+const originalLoad = Module._load;
+function patchRequire(globalAtom) {
+  globalAtom.packages.serviceHub.consume('pulsar.api', '0.1.0', apiConsumer);
+  Module._load = function(request, parent, isMain) {
+    const fromProvider = apis.get(request)
+    if (fromProvider) {
+      return fromProvider.exports || fromProvider.exportFunction()
+    }
+    return originalLoad(request, parent, isMain);
+  };
 }
 
 module.exports = { requireModule, addAtomExport };
