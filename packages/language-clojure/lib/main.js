674ffef8ef8a4ac2dafa19d6366a845886bd6f80
Maurício Szabo <mauricio.szabo@gmail.com>
2023-06-02 18:57:59 -0300

    Fixes on "comment block" for Clojure grammar

--- packages/language-clojure/lib/main.js
+++ packages/language-clojure/lib/main.js
@@ -4,7 +4,14 @@ exports.activate = function() {
   atom.grammars.addInjectionPoint('source.clojure', {
     type: 'quoting_lit',
     language: () => 'source-edn',
-    content: (node) => node,
+    content: (node) => {
+      let parent = node.parent
+      while(parent) {
+        if(parent.type === 'dis_expr') return null
+        parent = parent.parent
+      }
+      return node
+    },
     includeChildren: true,
     languageScope: 'source.edn',
     coverShallowerScopes: true
