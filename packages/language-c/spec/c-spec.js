b40c1e3f5e6d9c3c0638fdf87a1ea36e21828472
confused-Techie <dev@lhbasics.com>
2023-07-08 17:59:45 -0700

    `language-c` remove decaf suggestions

--- packages/language-c/spec/c-spec.js
+++ packages/language-c/spec/c-spec.js
@@ -1,11 +1,4 @@
-/*
- * decaffeinate suggestions:
- * DS101: Remove unnecessary use of Array.from
- * DS102: Remove unnecessary code created because of implicit returns
- * DS205: Consider reworking code to avoid use of IIFEs
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
+
 let TextEditor = null;
 const buildTextEditor = function(params) {
   if (atom.workspace.buildTextEditor != null) {
