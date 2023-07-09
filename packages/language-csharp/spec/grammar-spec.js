016aa0844974bd63f40ebffe7e44ef637d5948dc
confused-Techie <dev@lhbasics.com>
2023-07-08 18:06:23 -0700

    Decaf `language-csharp`

--- packages/language-csharp/spec/grammar-spec.js
+++ packages/language-csharp/spec/grammar-spec.js
@@ -1,8 +1,4 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
+
 describe("Language C# package", function() {
 
   beforeEach(() => waitsForPromise(() => atom.packages.activatePackage("language-csharp")));
@@ -10,12 +6,12 @@ describe("Language C# package", function() {
   describe("C# Script grammar", () => it("parses the grammar", function() {
     const grammar = atom.grammars.grammarForScopeName("source.csx");
     expect(grammar).toBeDefined();
-    return expect(grammar.scopeName).toBe("source.csx");
+    expect(grammar.scopeName).toBe("source.csx");
   }));
 
-  return describe("C# Cake grammar", () => it("parses the grammar", function() {
+  describe("C# Cake grammar", () => it("parses the grammar", function() {
     const grammar = atom.grammars.grammarForScopeName("source.cake");
     expect(grammar).toBeDefined();
-    return expect(grammar.scopeName).toBe("source.cake");
+    expect(grammar.scopeName).toBe("source.cake");
   }));
 });
