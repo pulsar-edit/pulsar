effb7b381bafcd046e471a42cf61a38a52eddfa9
Andrew Dupont <github@andrewdupont.net>
2023-07-30 23:23:17 -0700

    Add the Tree-sitter Markdown grammar.
    Quite proud of this one — it uses an outer parser to identify front-matter and
    highlight it properly, then injects the Markdown parser into everything that
    isn't front matter.
    
    The scope names are quite different — because the scope names in the legacy
    Markdown grammar are awful — so I adapted the eight built-in syntax themes so
    that there are no major style differences.

---
+++ packages/language-gfm/grammars/tree-sitter/tree-sitter-markdown/indents.scm
@@ -0,0 +1,3 @@
+; Intentionally empty indents.scm. By and large, indentation level should be
+; manually controlled by the user in Markdown; the best thing we can do is stay
+; out of the user's way.
