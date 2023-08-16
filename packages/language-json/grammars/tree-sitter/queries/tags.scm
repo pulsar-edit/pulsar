5c8f6abd941ed2cc9991ada6a4f28d9e0a4f4968
Andrew Dupont <github@andrewdupont.net>
2023-08-16 09:34:27 -0700

    Tag properties as such in JSON's `tags.scm`

--- packages/language-json/grammars/tree-sitter/queries/tags.scm
+++ packages/language-json/grammars/tree-sitter/queries/tags.scm
@@ -4,6 +4,7 @@
 ; Nested keys try to prepend the symbol name of their parent's key.
 (pair key: (string (string_content) @name
   (#is-not? test.descendantOfType "array")
+  (#set! symbol.tag "property")
   (#set! symbol.prependSymbolForNode parent.parent.parent.previousNamedSibling.firstNamedChild)
   (#set! symbol.contextNode parent.parent.parent.previousNamedSibling.firstNamedChild)
   (#set! symbol.joiner ".")))
