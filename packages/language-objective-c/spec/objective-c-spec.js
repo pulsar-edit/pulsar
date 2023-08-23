8c698e26d98b3aa832f788e30e06894d1661ed0b
confused-Techie <dev@lhbasics.com>
2023-08-22 22:32:16 -0700

    Manual decaf `language-objective-c` spec

--- packages/language-objective-c/spec/objective-c-spec.js
+++ packages/language-objective-c/spec/objective-c-spec.js
@@ -1,26 +1,22 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
-describe('Language-Objective-C', function() {
+
+describe('Language-Objective-C', () => {
   let grammar = null;
 
-  beforeEach(function() {
+  beforeEach(() => {
     waitsForPromise(() => atom.packages.activatePackage('language-objective-c'));
 
-    return waitsForPromise(() => atom.packages.activatePackage('language-c'));
+    waitsForPromise(() => atom.packages.activatePackage('language-c'));
   });
 
-  describe("Objective-C", function() {
+  describe("Objective-C", () => {
     beforeEach(() => grammar = atom.grammars.grammarForScopeName('source.objc'));
 
-    it('parses the grammar', function() {
+    it('parses the grammar', () => {
       expect(grammar).toBeTruthy();
-      return expect(grammar.scopeName).toBe('source.objc');
+      expect(grammar.scopeName).toBe('source.objc');
     });
 
-    return it('tokenizes classes', function() {
+    it('tokenizes classes', () => {
       const lines = grammar.tokenizeLines(`\
 @interface Thing
 @property (nonatomic, strong) NSArray *items;
@@ -29,19 +25,19 @@ describe('Language-Objective-C', function() {
       );
 
       expect(lines[0][1]).toEqual({value: 'interface', scopes: ["source.objc", "meta.interface-or-protocol.objc", "storage.type.objc"]});
-      return expect(lines[0][3]).toEqual({value: 'Thing', scopes: ["source.objc", "meta.interface-or-protocol.objc", "entity.name.type.objc"]});
+      expect(lines[0][3]).toEqual({value: 'Thing', scopes: ["source.objc", "meta.interface-or-protocol.objc", "entity.name.type.objc"]});
   });
 });
 
-  return describe("Objective-C++", function() {
+  describe("Objective-C++", () => {
     beforeEach(() => grammar = atom.grammars.grammarForScopeName('source.objcpp'));
 
-    it('parses the grammar', function() {
+    it('parses the grammar', () => {
       expect(grammar).toBeTruthy();
       return expect(grammar.scopeName).toBe('source.objcpp');
     });
 
-    return it('tokenizes classes', function() {
+    it('tokenizes classes', () => {
       const lines = grammar.tokenizeLines(`\
 class Thing1 {
   vector<int> items;
@@ -54,7 +50,7 @@ class Thing1 {
       );
 
       expect(lines[0][2].value).toBe('Thing1');
-      return expect(lines[4][3]).toEqual({value: 'Thing2', scopes: ["source.objcpp", "meta.interface-or-protocol.objc", "entity.name.type.objc"]});
+      expect(lines[4][3]).toEqual({value: 'Thing2', scopes: ["source.objcpp", "meta.interface-or-protocol.objc", "entity.name.type.objc"]});
   });
 });
 });
