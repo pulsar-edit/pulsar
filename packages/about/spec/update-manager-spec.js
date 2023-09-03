3cf49489cd889d51de9ab67d02c923613298f6cc
confused-Techie <dev@lhbasics.com>
2023-09-03 16:32:17 -0700

    Update specs, remove release channel testing, now that the logic has been fully removed

--- packages/about/spec/update-manager-spec.js
+++ packages/about/spec/update-manager-spec.js
@@ -16,19 +16,10 @@ describe('UpdateManager', () => {
 
     it('returns the page for the release when not a dev version', () => {
       expect(updateManager.getReleaseNotesURLForVersion('1.100.0')).toContain(
-        'pulsar-edit/pulsar/releases/tag/v1.100.0'
+        'pulsar-edit/pulsar/blob/master/CHANGELOG.md#11000'
       );
       expect(updateManager.getReleaseNotesURLForVersion('v1.100.0')).toContain(
-        'pulsar-edit/pulsar/releases/tag/v1.100.0'
-      );
-      // TODO: Since we no longer follow release channels, is it useful to continue testing their state?
-      expect(
-        updateManager.getReleaseNotesURLForVersion('1.100.0-beta10')
-      ).toContain('pulsar-edit/pulsar/releases/tag/v1.100.0-beta10');
-      expect(
-        updateManager.getReleaseNotesURLForVersion('1.100.0-nightly10')
-      ).toContain(
-        'pulsar-edit/pulsar-nightly-releases/releases/tag/v1.100.0-nightly10'
+        'pulsar-edit/pulsar/blob/master/CHANGELOG.md#11000'
       );
     });
   });
