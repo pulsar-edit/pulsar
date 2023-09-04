4e7f8ba7de25decf1185e13db89b0fb0cd3cda12
confused-Techie <dev@lhbasics.com>
2023-09-04 04:13:29 -0700

    Properly export `default-file-icons.js`

--- packages/tree-view/lib/default-file-icons.js
+++ packages/tree-view/lib/default-file-icons.js
@@ -1,7 +1,6 @@
 const fs = require('fs-plus');
 const path = require('path');
 
-module.exports =
 class DefaultFileIcons {
   iconClassForPath(filePath) {
     const extension = path.extname(filePath);
@@ -23,3 +22,5 @@ class DefaultFileIcons {
     }
   }
 }
+
+module.exports = new DefaultFileIcons();
