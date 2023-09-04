dd6f862226961260c559ad14a3248c8c17866ac9
confused-Techie <dev@lhbasics.com>
2023-09-03 20:02:53 -0700

    Remove spec for dev release notes

--- packages/about/spec/update-manager-spec.js
+++ packages/about/spec/update-manager-spec.js
@@ -8,12 +8,6 @@ describe('UpdateManager', () => {
   });
 
   describe('::getReleaseNotesURLForVersion', () => {
-    it('returns pulsar-edit download page when dev version', () => {
-      expect(
-        updateManager.getReleaseNotesURLForVersion('1.7.0-dev-e44b57d')
-      ).toContain('pulsar-edit.dev/download');
-    });
-
     it('returns the page for the release when not a dev version', () => {
       expect(updateManager.getReleaseNotesURLForVersion('1.100.0')).toContain(
         'pulsar-edit/pulsar/blob/master/CHANGELOG.md#11000'
