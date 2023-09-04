94a24dbec32a487eb70d66e0d9c225a7a2067890
confused-Techie <dev@lhbasics.com>
2023-09-03 20:33:49 -0700

    Add extra tests for good measure

--- packages/about/spec/update-manager-spec.js
+++ packages/about/spec/update-manager-spec.js
@@ -8,7 +8,15 @@ describe('UpdateManager', () => {
   });
 
   describe('::getReleaseNotesURLForVersion', () => {
+    it('returns the page for the release even when a dev version', () => {
+      expect(updateManager.getReleaseNotesURLForVersion('1.100.0-dev')).toContain(
+        'pulsar-edit/pulsar/blob/master/CHANGELOG.md#11000-dev'
+      );
+    });
     it('returns the page for the release when not a dev version', () => {
+      expect(updateManager.getReleaseNotesURLForVersion('1.108.2023090322')).toContain(
+        'pulsar-edit/pulsar/blob/master/CHANGELOG.md#11082023090322'
+      );
       expect(updateManager.getReleaseNotesURLForVersion('1.100.0')).toContain(
         'pulsar-edit/pulsar/blob/master/CHANGELOG.md#11000'
       );
