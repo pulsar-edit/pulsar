f4e78467a74a7fa340d624060d6629dbbe21dc09
Maurício Szabo <mauricio.szabo@gmail.com>
2023-09-21 00:54:48 -0300

    Simplifying queries for Clojure

--- packages/language-clojure/grammars/ts/edn-highlights.scm
+++ packages/language-clojure/grammars/ts/edn-highlights.scm
@@ -1,9 +1,4 @@
 ;; Collections
-(list_lit
- "(" @punctuation.section.list.begin (#is-not? test.descendantOfNodeWithData "clojure.dismissTag")
- ")" @punctuation.section.list.end)
-@meta.list
-
 (vec_lit
  "[" @punctuation.section.vector.begin (#is-not? test.descendantOfNodeWithData "clojure.dismissTag")
  "]" @punctuation.section.vector.end)
