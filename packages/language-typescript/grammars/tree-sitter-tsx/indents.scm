0b51a77cd6c0cc0636947b5d88c705f30dbd2e76
Andrew Dupont <github@andrewdupont.net>
2023-12-25 17:54:18 -0600

    [tree-sitter] Further JSX indent fixes for JS and TS alike

--- packages/language-typescript/grammars/tree-sitter-tsx/indents.scm
+++ packages/language-typescript/grammars/tree-sitter-tsx/indents.scm
@@ -3,22 +3,11 @@
 ; ===
 
 (jsx_opening_element [">"] @indent)
-
-; Support things like…
-; <div
-;   className={'foo'}
-;   onClick={onClick}
-; >
 (jsx_opening_element ["<"] @indent
-  (#is-not? test.startsOnSameRowAs parent.lastChild.startPosition))
+  (#is-not? test.startsOnSameRowAs parent.lastChild))
 ((jsx_opening_element [">"] @dedent)
-  (#is-not? test.startsOnSameRowAs parent.firstChild.startPosition))
+  (#is-not? test.startsOnSameRowAs parent.firstChild))
 
-; We match on the whole node here because
-; (a) we need to please the dedent-while-typing heuristic, so the line text
-;     must match the capture node's text; and
-; (b) we don't want the dedent to trigger until the very last `>` has been
-;     typed.
 (jsx_closing_element ">") @dedent
 
 ; Support things like…
@@ -28,13 +17,13 @@
 ;   alt={'foo'}
 ; />
 (jsx_self_closing_element "<" @indent
-  (#is-not? test.startsOnSameRowAs parent.lastChild.startPosition))
+  (#is-not? test.startsOnSameRowAs parent.lastChild))
 
 ; There isn't a single node whose exact text will match the line content at any
 ; point, so the usual heuristic won't work. Instead we set `indent.force` and
 ; use `test.lastTextOnRow` to ensure that the dedent fires exactly once while
 ; typing.
 ((jsx_self_closing_element ">" @dedent)
-  (#is-not? test.startsOnSameRowAs parent.firstChild.startPosition)
+  (#is-not? test.startsOnSameRowAs parent.firstChild)
   (#is? test.lastTextOnRow)
   (#set! indent.force true))
