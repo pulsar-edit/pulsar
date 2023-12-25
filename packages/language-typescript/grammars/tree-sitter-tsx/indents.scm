bc55c4fce3ee26b4c5d83b7dca93f8994e32e42c
Andrew Dupont <github@andrewdupont.net>
2023-12-25 17:54:18 -0600

    [tree-sitter] Further JSX indent fixes for JS and TS alike

--- packages/language-typescript/grammars/tree-sitter-tsx/indents.scm
+++ packages/language-typescript/grammars/tree-sitter-tsx/indents.scm
@@ -2,7 +2,28 @@
 ; JSX
 ; ===
 
-(jsx_opening_element ["<" ">"] @indent)
-(jsx_opening_element [">"] @dedent)
+(jsx_opening_element [">"] @indent)
+(jsx_opening_element ["<"] @indent
+  (#is-not? test.startsOnSameRowAs parent.lastChild))
+((jsx_opening_element [">"] @dedent)
+  (#is-not? test.startsOnSameRowAs parent.firstChild))
 
-(jsx_closing_element ">" @dedent)
+(jsx_closing_element ">") @dedent
+
+; Support things like…
+; <img
+;   height={40}
+;   width={40}
+;   alt={'foo'}
+; />
+(jsx_self_closing_element "<" @indent
+  (#is-not? test.startsOnSameRowAs parent.lastChild))
+
+; There isn't a single node whose exact text will match the line content at any
+; point, so the usual heuristic won't work. Instead we set `indent.force` and
+; use `test.lastTextOnRow` to ensure that the dedent fires exactly once while
+; typing.
+((jsx_self_closing_element ">" @dedent)
+  (#is-not? test.startsOnSameRowAs parent.firstChild)
+  (#is? test.lastTextOnRow)
+  (#set! indent.force true))
