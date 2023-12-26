7b4a50d2b50f657ba4a22c3dc3c59cdcb3bbf526
Andrew Dupont <github@andrewdupont.net>
2023-12-25 18:04:44 -0600

    (oops)

--- packages/language-javascript/grammars/ts/indents.scm
+++ packages/language-javascript/grammars/ts/indents.scm
@@ -162,9 +162,9 @@
 ;   onClick={onClick}
 ; >
 (jsx_opening_element ["<"] @indent
-  (#is-not? test.startsOnSameRowAs parent.lastChild))
+  (#is-not? test.startsOnSameRowAs parent.lastChild.startPosition))
 ((jsx_opening_element [">"] @dedent)
-  (#is-not? test.startsOnSameRowAs parent.firstChild))
+  (#is-not? test.startsOnSameRowAs parent.firstChild.startPosition))
 
 ; We match on the whole node here because
 ; (a) we need to please the dedent-while-typing heuristic, so the line text
@@ -180,13 +180,13 @@
 ;   alt={'foo'}
 ; />
 (jsx_self_closing_element "<" @indent
-  (#is-not? test.startsOnSameRowAs parent.lastChild))
+  (#is-not? test.startsOnSameRowAs parent.lastChild.startPosition))
 
 ; There isn't a single node whose exact text will match the line content at any
 ; point, so the usual heuristic won't work. Instead we set `indent.force` and
 ; use `test.lastTextOnRow` to ensure that the dedent fires exactly once while
 ; typing.
 ((jsx_self_closing_element ">" @dedent)
-  (#is-not? test.startsOnSameRowAs parent.firstChild)
+  (#is-not? test.startsOnSameRowAs parent.firstChild.startPosition)
   (#is? test.lastTextOnRow)
   (#set! indent.force true))
