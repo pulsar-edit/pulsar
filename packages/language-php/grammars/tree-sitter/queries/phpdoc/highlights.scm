18788c095ef38642506a188007fffffed81adc73
Andrew Dupont <github@andrewdupont.net>
2024-01-08 15:49:22 -0800

    Address feedback on PHP syntax highlighting

--- packages/language-php/grammars/tree-sitter/queries/phpdoc/highlights.scm
+++ packages/language-php/grammars/tree-sitter/queries/phpdoc/highlights.scm
@@ -5,12 +5,9 @@
 ((document) @punctuation.definition.end.comment.phpdoc.php
   (#set! adjust.startAndEndAroundFirstMatchOf "(?:\\*)?\\*/$"))
 
-(tag_name) @storage.type.class.phpdoc.php
-
-(tag (type_list) @entity.other.type.instance.phpdoc.php)
-
+(tag_name) @entity.name.tag.phpdoc.php
+(named_type) @storage.type.instance.phpdoc.php
 (variable_name) @variable.other.phpdoc.php
-
 (uri) @markup.underline.link.phpdoc.php
 
 (inline_tag "{" @punctation.definition.tag.begin.brace.curly.phpdoc.php)
