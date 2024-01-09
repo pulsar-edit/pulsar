bbf4cb441750c4e6464a061780af59f0cbf47d0c
Andrew Dupont <github@andrewdupont.net>
2024-01-08 17:33:34 -0800

    Add TODO and hyperlink injections to C/C++

--- packages/language-c/lib/main.js
+++ packages/language-c/lib/main.js
@@ -1,15 +1,38 @@
-exports.activate = function() {
+exports.activate = function () {
   // Highlight macro bodies as C/C++
   for (const language of ['c', 'cpp']) {
     for (const nodeType of ['preproc_def', 'preproc_function_def']) {
       atom.grammars.addInjectionPoint(`source.${language}`, {
         type: nodeType,
-        language(node) {
+        language() {
           return language;
         },
         content(node) {
           return node.lastNamedChild;
         }
+      });
+    }
+
+    const TODO_PATTERN = /\b(TODO|FIXME|CHANGED|XXX|IDEA|HACK|NOTE|REVIEW|NB|BUG|QUESTION|COMBAK|TEMP|DEBUG|OPTIMIZE|WARNING)\b/;
+    const HYPERLINK_PATTERN = /\bhttps?:/
+
+    atom.grammars.addInjectionPoint(`source.${language}`, {
+      type: 'comment',
+      language: (node) => {
+        return TODO_PATTERN.test(node.text) ? 'todo' : undefined;
+      },
+      content: (node) => node,
+      languageScope: null
+    });
+
+    for (let type of ['string_literal', 'comment']) {
+      atom.grammars.addInjectionPoint(`source.${language}`, {
+        type,
+        language: (node) => {
+          return HYPERLINK_PATTERN.test(node.text) ? 'hyperlink' : undefined;
+        },
+        content: (node) => node,
+        languageScope: null
       });
     }
   }
