3864c0d0f7457ea65cbf933941ff1b7ae56061d0
Andrew Dupont <github@andrewdupont.net>
2024-01-17 18:34:26 -0800

    [language-go] Update `tree-sitter-go` parser to latest…
    …and include some highlighting fixes from #869.

--- packages/language-go/grammars/tree-sitter-go/highlights.scm
+++ packages/language-go/grammars/tree-sitter-go/highlights.scm
@@ -47,6 +47,8 @@
 
 [
   "struct"
+  "interface"
+  "map"
 ] @storage.type._TYPE_.go
 
 (struct_type
@@ -55,13 +57,14 @@
       (field_identifier) @entity.other.attribute-name.go)))
 
 (keyed_element
-  (field_identifier) @entity.other.attribute-name.go
-  .
-  ":" @punctuation.separator.key-value.go)
+  . (literal_element) @entity.other.attribute-name.go)
+
+(keyed_element ":" @punctuation.separator.key-value.go)
 
 [
   "break"
   "case"
+  "chan"
   "continue"
   "default"
   "defer"
@@ -252,7 +255,8 @@
 
 ";" @punctuation.terminator.go
 "," @punctuation.separator.comma.go
-":" @punctuation.separator.colon.go
+(":" @punctuation.separator.colon.go
+  (#set! capture.shy))
 
 (parameter_list
   "(" @punctuation.definition.parameters.begin.bracket.round.go
