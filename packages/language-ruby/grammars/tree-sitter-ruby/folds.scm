73e9543eb796f06f3af4eae4304fe892a3fbea62
Maurício Szabo <mauricio.szabo@gmail.com>
2024-03-16 02:15:06 -0300

    Fixed folds for Ruby

--- packages/language-ruby/grammars/tree-sitter-ruby/folds.scm
+++ packages/language-ruby/grammars/tree-sitter-ruby/folds.scm
@@ -1,13 +1,16 @@
 [
   (method)
+  (comment)
   (singleton_method)
   (class)
   (module)
   (case)
   (do_block)
+  (block)
   (singleton_class)
   (lambda)
   (hash)
+  (argument_list)
   (array)
 ] @fold
 
@@ -15,14 +18,19 @@
 ; Fold from `if` to the next `elsif` or `else` in the chain.
 ((if
   alternative: [(elsif) (else)]) @fold
-  (#set! fold.endAt firstNamedChild.nextNamedSibling.nextNamedSibling.startPosition)
-  (#set! fold.adjustToEndOfPreviousLine true))
+  (#set! fold.endAt lastNamedChild.startPosition)
+  (#set! fold.adjustToEndOfPreviousRow true))
+
+((unless
+  alternative: (else)) @fold
+  (#set! fold.endAt lastNamedChild.startPosition)
+  (#set! fold.adjustToEndOfPreviousRow true))
 
 ; Fold from `elsif` to the next `elsif` or `else` in the chain.
 ((elsif
   consequence: [(then) (elsif)]) @fold
-  (#set! fold.endAt firstNamedChild.nextNamedSibling.nextNamedSibling.startPosition)
-  (#set! fold.adjustToEndOfPreviousLine true))
+  (#set! fold.endAt lastNamedChild.startPosition)
+  (#set! fold.adjustToEndOfPreviousRow true))
 
 ; Fold from `else` to `end`.
 ((else) @fold
@@ -30,3 +38,4 @@
 
 ; A bare `if` without an `else` or `elsif`.
 (if) @fold
+(unless) @fold
