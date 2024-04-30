bd5d1f69aefcaf1735b1ef9d41d758689dd9020b
Maurício Szabo <mauricio.szabo@gmail.com>
2024-04-30 12:32:47 -0300

    Fixed Javascript injections for Clojure

--- packages/language-clojure/lib/main.js
+++ packages/language-clojure/lib/main.js
@@ -34,20 +34,17 @@ exports.activate = function() {
     coverShallowerScopes: true
   });
 
-  const checkFormCall = (specialFormText, node) => {
-    let parent = node.parent
-    let grandparent = parent?.parent
-    return grandparent?.type === 'list_lit' &&
-      grandparent.children[1].text === specialFormText &&
-      grandparent.children[2].id === parent.id
-  }
-
   atom.grammars.addInjectionPoint('source.clojure', {
-    type: 'str_content',
-    language: (node) => checkFormCall('js*', node) && 'javascript',
-    content: notDisChild,
-    includeChildren: true,
-    languageScope: 'source.js',
-    coverShallowerScopes: true
+    type: 'list_lit',
+    language(node) {
+      if(node.children[1].text === 'js*') {
+        return 'javascript'
+      }
+    },
+    content(node) {
+      if(node.children[2].type === 'str_lit') {
+        return node.children[2].children[1];
+      }
+    },
   });
 }
