67857f8875524a2d58e0548393df366ff2b1a9f8
Andrew Dupont <github@andrewdupont.net>
2024-09-22 13:35:15 -0700

    Un-focus a spec

--- packages/language-c/spec/tree-sitter-grammar-spec.js
+++ packages/language-c/spec/tree-sitter-grammar-spec.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-fdescribe('WASM Tree-sitter C grammar', () => {
+describe('WASM Tree-sitter C grammar', () => {
 
   beforeEach(async () => {
     await atom.packages.activatePackage('language-c');
