42e73f695c02e538e0534af75941470d238ee206
Andrew Dupont <github@andrewdupont.net>
2025-02-16 15:39:15 -0800

    [find-and-replace] Don't use `requestAnimationFrame`…
    …in the `waitForCondition` helper.

--- packages/find-and-replace/spec/helpers.js
+++ packages/find-and-replace/spec/helpers.js
@@ -2,16 +2,13 @@ function waitForCondition(fn) {
   return new Promise((resolve, reject) => {
     const interval = setInterval(() => {
       if (fn()) {
-        requestAnimationFrame(resolve);
-        // resolve();
+        resolve();
         clearInterval(interval);
         clearTimeout(timeout);
       }
     }, 100);
     let timeout = setTimeout(() => {
-      requestAnimationFrame(() => {
-        reject(new Error(`Timeout waiting for condition`));
-      })
+      reject(new Error(`Timeout waiting for condition`));
       clearInterval(interval);
     }, 4000);
   });
