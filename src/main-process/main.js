a77026782f817c9c4f086066910ea1df8b18119c
Vsevolod Shoidlin <123181257+Pixelithas@users.noreply.github.com>
2025-03-10 18:48:34 +0800

    Update main.js

--- src/main-process/main.js
+++ src/main-process/main.js
@@ -6,7 +6,35 @@ const path = require('path');
 const fs = require('fs-plus');
 const CSON = require('season');
 const yargs = require('yargs');
-const { app } = require('electron');
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    transparent: false,
+    backgroundColor: '#FFF',
+    autoHideMenuBar: true,
+  });
+
+  mainWindow.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
 
 const args = yargs(process.argv)
   // Don't handle --help or --version here; they will be handled later.
