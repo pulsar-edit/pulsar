71b91d80a458f2d6af6f554f8fd78efd898fb839
Andrew Dupont <github@andrewdupont.net>
2025-03-25 00:13:03 -0700

    Refactor `StateStore` to remove dependency on the `atom` global

--- spec/state-store-spec.js
+++ spec/state-store-spec.js
@@ -12,47 +12,51 @@ describe('StateStore', () => {
       atom.config.set('core.useLegacySessionStore', true)
     })
 
-    it('can save, load, and delete states', () => {
-      const store = new StateStore(databaseName, version);
-      return store
-        .save('key', { foo: 'bar' })
-        .then(() => store.load('key'))
-        .then(state => {
-          expect(state).toEqual({ foo: 'bar' });
-        })
-        .then(() => store.delete('key'))
-        .then(() => store.load('key'))
-        .then(value => {
-          expect(value).toBeNull();
-        })
-        .then(() => store.count())
-        .then(count => {
-          expect(count).toBe(0);
-        });
+    it('can save, load, and delete states', async () => {
+      const store = new StateStore(databaseName, version, {
+        config: atom.config
+      });
+      store.initialize({ configDirPath: atom.getConfigDirPath() });
+
+      await store.save('key', { foo: 'bar' });
+
+      let state = await store.load('key');
+      expect(state).toEqual({ foo: 'bar' });
+
+      await store.delete('key');
+
+      expect(await store.load('key')).toBeNull();
+      expect(await store.count()).toBe(0);
     });
 
     it('resolves with null when a non-existent key is loaded', () => {
-      const store = new StateStore(databaseName, version);
+      const store = new StateStore(databaseName, version, {
+        config: atom.config
+      });
+      store.initialize({ configDirPath: atom.getConfigDirPath() });
       return store.load('no-such-key').then(value => {
         expect(value).toBeNull();
       });
     });
 
-    it('can clear the state object store', () => {
-      const store = new StateStore(databaseName, version);
-      return store
-        .save('key', { foo: 'bar' })
-        .then(() => store.count())
-        .then(count => expect(count).toBe(1))
-        .then(() => store.clear())
-        .then(() => store.count())
-        .then(count => {
-          expect(count).toBe(0);
-        });
+    it('can clear the state object store', async () => {
+      const store = new StateStore(databaseName, version, {
+        config: atom.config
+      });
+      store.initialize({ configDirPath: atom.getConfigDirPath() });
+
+      await store.save('key', { foo: 'bar' });
+      expect(await store.count()).toBe(1);
+
+      await store.clear();
+      expect(await store.count()).toBe(0);
     });
 
     it('returns a database instance via dbPromise', async () => {
-      const store = new StateStore(databaseName, version);
+      const store = new StateStore(databaseName, version, {
+        config: atom.config
+      });
+      store.initialize({ configDirPath: atom.getConfigDirPath() });
       const instance = await store.dbPromise;
       expect(instance instanceof IDBDatabase).toBe(true);
     });
@@ -60,13 +64,16 @@ describe('StateStore', () => {
     describe('when there is an error reading from the database', () => {
       it('rejects the promise returned by load', async () => {
         jasmine.useRealClock();
-        const store = new StateStore(databaseName, version);
+        const store = new StateStore(databaseName, version, {
+          config: atom.config
+        });
+        store.initialize({ configDirPath: atom.getConfigDirPath() });
 
         const fakeErrorEvent = {
           target: { errorCode: 'Something bad happened' }
         };
 
-        spyOn(IDBObjectStore.prototype, 'get').and.callFake(key => {
+        spyOn(IDBObjectStore.prototype, 'get').and.callFake(_key => {
           let request = {};
           process.nextTick(() => request.onerror(fakeErrorEvent));
           return request;
@@ -88,47 +95,51 @@ describe('StateStore', () => {
       atom.config.set('core.useLegacySessionStore', false)
     })
 
-    it('can save, load, and delete states', () => {
-      const store = new StateStore(databaseName, version);
-      return store
-        .save('key', { foo: 'bar' })
-        .then(() => store.load('key'))
-        .then(state => {
-          expect(state).toEqual({ foo: 'bar' });
-        })
-        .then(() => store.delete('key'))
-        .then(() => store.load('key'))
-        .then(value => {
-          expect(value).toBeNull();
-        })
-        .then(() => store.count())
-        .then(count => {
-          expect(count).toBe(0);
-        });
+    it('can save, load, and delete states', async () => {
+      const store = new StateStore(databaseName, version, {
+        config: atom.config
+      });
+      store.initialize({ configDirPath: atom.getConfigDirPath() });
+
+      await store.save('key', { foo: 'bar' });
+
+      let state = await store.load('key');
+      expect(state).toEqual({ foo: 'bar' });
+
+      await store.delete('key');
+
+      expect(await store.load('key')).toBeNull();
+      expect(await store.count()).toBe(0);
     });
 
     it('resolves with null when a non-existent key is loaded', () => {
-      const store = new StateStore(databaseName, version);
+      const store = new StateStore(databaseName, version, {
+        config: atom.config
+      });
+      store.initialize({ configDirPath: atom.getConfigDirPath() });
       return store.load('no-such-key').then(value => {
         expect(value).toBeNull();
       });
     });
 
-    it('can clear the state object store', () => {
-      const store = new StateStore(databaseName, version);
-      return store
-        .save('key', { foo: 'bar' })
-        .then(() => store.count())
-        .then(count => expect(count).toBe(1))
-        .then(() => store.clear())
-        .then(() => store.count())
-        .then(count => {
-          expect(count).toBe(0);
-        });
+    it('can clear the state object store', async () => {
+      const store = new StateStore(databaseName, version, {
+        config: atom.config
+      });
+      store.initialize({ configDirPath: atom.getConfigDirPath() });
+
+      await store.save('key', { foo: 'bar' });
+      expect(await store.count()).toBe(1);
+
+      await store.clear();
+      expect(await store.count()).toBe(0);
     });
 
     it('returns a database instance via dbPromise', async () => {
-      const store = new StateStore(databaseName, version);
+      const store = new StateStore(databaseName, version, {
+        config: atom.config
+      });
+      store.initialize({ configDirPath: atom.getConfigDirPath() });
       const instance = await store.dbPromise;
       const Database = require("better-sqlite3");
       expect(instance instanceof Database).toBe(true);
--- src/state-store/sql.js
+++ src/state-store/sql.js
@@ -10,112 +10,104 @@ const nativeSQLite = require(path.join(
 const sqlite3 = require('better-sqlite3');
 
 module.exports = class SQLStateStore {
-  constructor(databaseName, version) {
-    const table = databaseName + version;
-    this.tableName = '"' + table + '"';
-    this.dbPromise = (async () => {
-      await awaitForAtomGlobal();
-      const dbPath = path.join(atom.getConfigDirPath(), 'session-store.db');
-      let db;
-      try {
-        db = sqlite3(dbPath, {nativeBinding: nativeSQLite});
-      } catch(error) {
-        atom.notifications.addFatalError('Error loading database', {
-          stack: new Error('Error loading SQLite database for state storage').stack,
-          dismissable: true
-        });
-        console.error('Error loading SQLite database', error);
-        return null;
-      }
-      db.pragma('journal_mode = WAL');
-      db.exec(
-        `CREATE TABLE IF NOT EXISTS ${this.tableName} (key VARCHAR, value JSON)`
+  constructor(databaseName, version, { storagePath }) {
+    const table = `${databaseName}${version}`;
+    this.tableName = `"${table}"`;
+
+    let dbPath = path.join(storagePath, 'session-store.db');
+    let db;
+    try {
+      db = sqlite3(dbPath, { nativeBinding: nativeSQLite });
+    } catch (error) {
+      let stack = new Error('Error loading SQLite database for state storage').stack;
+      atom.notifications.addFatalError(
+        'Error loading database',
+        { stack, dismissable: true }
       );
-      db.exec(
-        `CREATE UNIQUE INDEX IF NOT EXISTS "${table}_index" ON ${this.tableName}(key)`
-      );
-      return db;
-    })();
-    this.connected = false;
-    this.dbPromise.then(db => this.connected = !!db);
+      console.error('Error loading SQLite database', error);
+      return null;
+    }
+
+    db.pragma('journal_mode = WAL');
+    db.exec(
+      `CREATE TABLE IF NOT EXISTS ${this.tableName} (key VARCHAR, value JSON)`
+    );
+    db.exec(
+      `CREATE UNIQUE INDEX IF NOT EXISTS "${table}_index" ON ${this.tableName}(key)`
+    );
+
+    this.db = db;
+    this.connected = true;
+  }
+
+  // `better-sqlite3` offers a synchronous API and is militant about why it’s
+  // actually better for performance. But the contract for this adapter expects
+  // us to return promises here, so we mark all these functions as `async` so
+  // that they'll implicitly wrap return values in `Promise.resolve`.
+  get dbPromise() {
+    return Promise.resolve(this.db);
   }
 
   isConnected() {
     return this.connected;
   }
 
-  connect() {
-    return this.dbPromise.then(db => !!db);
+  async connect() {
+    return true;
   }
 
-  save(key, value) {
-    return this.dbPromise.then(db => {
-      if(!db) return null;
-      return exec(db,
-        `REPLACE INTO ${this.tableName} VALUES (?, ?)`,
-        key,
-        JSON.stringify({ value: value, storedAt: new Date().toString() })
-      );
-    });
-  }
-
-  load(key) {
-    return this.dbPromise.then(db => {
-      if(!db) return null;
-      return getOne(db, `SELECT value FROM ${this.tableName} WHERE key = ?`, key);
-    }).then(result => {
-      if(result) {
-        const parsed = JSON.parse(result.value, reviver);
-        return parsed?.value;
-      }
-      return null;
-    });
-  }
-
-  delete(key) {
-    return this.dbPromise.then(db =>
-      exec(db, `DELETE FROM ${this.tableName} WHERE key = ?`, key)
+  async save(key, value) {
+    if (!this.db) return null;
+    return exec(
+      this.db,
+      `REPLACE INTO ${this.tableName} VALUES (?, ?)`,
+      key,
+      JSON.stringify({ value: value, storedAt: new Date().toString() })
     );
   }
 
-  clear() {
-    return this.dbPromise.then(db =>
-      exec(db, `DELETE FROM ${this.tableName}`)
+  async load(key) {
+    if (!this.db) return null;
+    let result = getOne(
+      this.db,
+      `SELECT value FROM ${this.tableName} WHERE key = ?`,
+      key
     );
+    if (!result) return null;
+    let parsed = JSON.parse(result.value, reviver);
+    return parsed?.value;
   }
 
-  count() {
-    return this.dbPromise.then(db => {
-      if(!db) return null;
-      const r = getOne(db, `SELECT COUNT(key) c FROM ${this.tableName}`);
-      return r.c;
-    });
+  async delete(key) {
+    exec(this.db, `DELETE FROM ${this.tableName} WHERE key = ?`, key);
+  }
+
+  async clear() {
+    exec(this.db, `DELETE from ${this.tableName}`);
+  }
+
+  async count() {
+    if (!this.db) return null;
+    let result = getOne(
+      this.db,
+      `SELECT COUNT(key) itemCount FROM ${this.tableName}`
+    );
+    return result.itemCount;
   }
 };
 
 function getOne(db, sql, ...params) {
   const stmt = db.prepare(sql);
-  return stmt.get(params)
+  return stmt.get(params);
 }
 
 function exec(db, sql, ...params) {
   const stmt = db.prepare(sql);
-  stmt.run(params)
-}
-
-function awaitForAtomGlobal() {
-  return new Promise(resolve => {
-    const i = setInterval(() => {
-      if(atom) {
-        clearInterval(i);
-        resolve();
-      }
-    }, 50);
-  })
+  return stmt.run(params);
 }
 
 function reviver(_, value) {
-  if(value?.type === 'Buffer') {
+  if (value?.type === 'Buffer') {
     return Buffer.from(value.data);
   } else {
     return value;
