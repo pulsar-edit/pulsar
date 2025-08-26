fea4d8cb44c734480b2b1dae6b333a48de25a334
Wes <5124946+wesinator@users.noreply.github.com>
2025-08-25 22:33:47 -0400

    update-manager-spec.js - release notes url spec change

--- packages/about/spec/update-manager-spec.js
+++ packages/about/spec/update-manager-spec.js
@@ -12,13 +12,13 @@ describe('UpdateManager', () => {
       expect(updateManager.getReleaseNotesURLForVersion('1.100.0-dev')).toContain(
         'pulsar-edit/pulsar/blob/master/CHANGELOG.md#11000-dev'
       );
-    });
-    it('returns the page for the release when not a dev version', () => {
       expect(updateManager.getReleaseNotesURLForVersion('1.108.2023090322')).toContain(
         'pulsar-edit/pulsar/blob/master/CHANGELOG.md#11082023090322'
       );
-      expect(updateManager.getReleaseNotesURLForVersion('1.100.0')).toContain(
-        'pulsar-edit/pulsar/blob/master/CHANGELOG.md#11000'
+    });
+    it('returns the page for the release when not a dev version', () => {
+      expect(updateManager.getReleaseNotesURLForVersion('1.129.0')).toContain(
+        'pulsar-edit/pulsar/blob/master/CHANGELOG.md#11290'
       );
       expect(updateManager.getReleaseNotesURLForVersion('v1.100.0')).toContain(
         'pulsar-edit/pulsar/blob/master/CHANGELOG.md#11000'
