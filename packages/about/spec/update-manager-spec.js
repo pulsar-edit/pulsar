c643f12cfedddf9820c7f55c338d7112f8d7fb0e
Wes <5124946+wesinator@users.noreply.github.com>
2025-08-30 20:48:27 -0400

    update-manager-spec.js - differentiate rolling and dev release tests

--- packages/about/spec/update-manager-spec.js
+++ packages/about/spec/update-manager-spec.js
@@ -12,10 +12,14 @@ describe('UpdateManager', () => {
       expect(updateManager.getReleaseNotesURLForVersion('1.100.0-dev')).toContain(
         'pulsar-edit/pulsar/blob/master/CHANGELOG.md#11000-dev'
       );
+    });
+
+    it('returns the page for the release when a rolling ("nightly") release version', () => {
       expect(updateManager.getReleaseNotesURLForVersion('1.108.2023090322')).toContain(
         'pulsar-edit/pulsar/blob/master/CHANGELOG.md#11082023090322'
       );
     });
+
     it('returns the page for the release when not a dev version', () => {
       expect(updateManager.getReleaseNotesURLForVersion('1.129.0')).toContain(
         'pulsar-edit/pulsar/blob/master/CHANGELOG.md#11290'
