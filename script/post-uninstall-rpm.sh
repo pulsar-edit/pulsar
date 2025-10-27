9a08fb53c6e3c279a100fc9a2e6b50db21c09ff5
Andrew Dupont <github@andrewdupont.net>
2025-10-27 16:25:37 -0700

    Preserve an `ATOM_CHANNEL` environment variable if it exists…
    …and other changes related to release-channel agnosticism.
    
    (These should've been included in an earlier commit.)

--- script/post-uninstall-rpm.sh
+++ script/post-uninstall-rpm.sh
@@ -7,14 +7,23 @@ if [ $1 -ge 1 ]; then
     exit 0
 fi
 
-PULSAR_SCRIPT_PATH='/usr/bin/pulsar'
+PULSAR_SCRIPT_PATH='/usr/bin/${executable}'
 
 if [ -f "$PULSAR_SCRIPT_PATH" ]
 then
   rm "$PULSAR_SCRIPT_PATH"
 fi
 
-PPM_SYMLINK_PATH='/usr/bin/ppm'
+case $executable in
+  pulsar-next)
+    ppm_executable=ppm-next
+    ;;
+  *)
+    ppm_executable=ppm
+    ;;
+esac
+
+PPM_SYMLINK_PATH='/usr/bin/${ppm_executable}'
 
 if [ -L "$PPM_SYMLINK_PATH" ]
 then
