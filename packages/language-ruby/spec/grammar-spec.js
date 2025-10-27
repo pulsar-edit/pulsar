0238bbe5982e5e5bc5a2cf0fa0aba9897d41809f
Andrew Dupont <github@andrewdupont.net>
2025-10-27 15:39:16 -0700

    [language-ruby] Add a basic grammar test spec

---
+++ packages/language-ruby/spec/grammar-spec.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const GRAMMAR_TEST_FILES = [
+  'syntax.rb'
+];
+
+describe('Grammar tests', () => {
+  beforeEach(async () => {
+    atom.config.set('core.useTreeSitterParsers', true);
+    await atom.packages.activatePackage("language-ruby");
+  });
+
+  it('passes grammar tests', async () => {
+    for (let file of GRAMMAR_TEST_FILES) {
+      await runGrammarTests(path.join(__dirname, 'fixtures', 'grammar', file), /#/)
+    }
+  });
+});
