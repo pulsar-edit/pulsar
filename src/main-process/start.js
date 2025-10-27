6c0b04da085754bf1b342e1b959342a2225b420e
Andrew Dupont <github@andrewdupont.net>
2025-10-27 16:16:07 -0700

    Delete deprecated "app.allowRendererProcessReuse = false"
    This option has been removed, and thus no-longer does anything,
    as of Electron 14.
    
    Committer note: This change was split out from the prior commit.

--- src/main-process/start.js
+++ src/main-process/start.js
@@ -35,9 +35,6 @@ module.exports = function start(resourcePath, devResourcePath, startTime) {
     }
   });
 
-  // TodoElectronIssue this should be set to true before Electron 12 - https://github.com/electron/electron/issues/18397
-  app.allowRendererProcessReuse = false;
-
   app.commandLine.appendSwitch('enable-experimental-web-platform-features');
 
   const args = parseCommandLine(process.argv.slice(1));
