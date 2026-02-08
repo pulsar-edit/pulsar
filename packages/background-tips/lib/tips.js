2f36656d97e1fe5976501df2f996099f29f09c7e
Sebastian Balcerowiak <asiloisad.93@gmail.com>
2026-02-08 01:10:38 +0100

    Add more default tips to fix "changes text" test with single-tip array

--- packages/background-tips/lib/tips.js
+++ packages/background-tips/lib/tips.js
@@ -1,3 +1,5 @@
 module.exports = [
   "Split your editor into multiple panes with {pane:split-right-and-copy-active-item}",
+  "You can move lines of text up and down with {editor:move-line-up} and {editor:move-line-down}",
+  "Toggle line comments quickly with {editor:toggle-line-comments}",
 ];
