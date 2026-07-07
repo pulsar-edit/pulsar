5f664a2bd7f13e9644c480f85615533e3ef71f09
Sebastian Balcerowiak <asiloisad.93@gmail.com>
2026-07-07 23:53:34 +0200

    fix(win-shell): use stable context menu identity

--- src/main-process/win-shell.js
+++ src/main-process/win-shell.js
@@ -1,20 +1,26 @@
 const Registry = require("winreg");
+const { execFile } = require("child_process");
 const Path = require("path");
 const { getAppName } = require("../get-app-details.js");
+const packageMetadata = require("../../package.json");
 
-const appName = getAppName();
+const appName =
+  packageMetadata.productName || packageMetadata.branding?.name || packageMetadata.name;
+const runtimeAppName = getAppName();
+const obsoleteAppNames = runtimeAppName !== appName ? [runtimeAppName] : [];
 const exeName = Path.basename(process.execPath);
 const appPath = `"${process.execPath}"`;
-const fileIconPath = `"${Path.join(process.execPath, "..", "resources", "lumine.ico")}"`;
+const fileIconPath = Path.join(process.execPath, "..", "resources", "lumine.ico");
 
 class ShellOption {
-  constructor(key, parts) {
+  constructor(key, parts, obsoleteKeys = []) {
     this.isRegistered = this.isRegistered.bind(this);
     this.register = this.register.bind(this);
     this.deregister = this.deregister.bind(this);
     this.update = this.update.bind(this);
     this.key = key;
     this.parts = parts;
+    this.obsoleteKeys = obsoleteKeys;
   }
 
   isRegistered(callback) {
@@ -29,24 +35,28 @@ class ShellOption {
   register(callback) {
     let doneCount = this.parts.length;
     this.parts.forEach((part) => {
-      let reg = new Registry({
-        hive: "HKCU",
-        key: part.key != null ? `${this.key}\\${part.key}` : this.key,
+      const keyPath = `HKCU${part.key != null ? `${this.key}\\${part.key}` : this.key}`;
+      const args = ["ADD", keyPath];
+      if (part.name === "") {
+        args.push("/ve");
+      } else {
+        args.push("/v", part.name);
+      }
+      args.push("/t", "REG_SZ", "/d", part.value, "/f");
+      execFile("reg.exe", args, { shell: false }, () => {
+        if (--doneCount === 0) return this.destroyObsoleteKeys(callback);
       });
-      return reg.create(() =>
-        reg.set(part.name, Registry.REG_SZ, part.value, () => {
-          if (--doneCount === 0) return callback();
-        }),
-      );
     });
   }
 
   deregister(callback) {
     this.isRegistered((isRegistered) => {
       if (isRegistered) {
-        new Registry({ hive: "HKCU", key: this.key }).destroy(() => callback(null, true));
+        new Registry({ hive: "HKCU", key: this.key }).destroy(() =>
+          this.destroyObsoleteKeys(() => callback(null, true)),
+        );
       } else {
-        callback(null, false);
+        this.destroyObsoleteKeys(() => callback(null, false));
       }
     });
   }
@@ -63,6 +73,17 @@ class ShellOption {
       }
     });
   }
+
+  destroyObsoleteKeys(callback) {
+    let doneCount = this.obsoleteKeys.length;
+    if (doneCount === 0) return callback();
+
+    this.obsoleteKeys.forEach((key) => {
+      new Registry({ hive: "HKCU", key }).destroy(() => {
+        if (--doneCount === 0) return callback();
+      });
+    });
+  }
 }
 
 exports.appName = appName;
@@ -76,18 +97,31 @@ exports.fileHandler = new ShellOption(`\\Software\\Classes\\Applications\\${exeN
 let contextParts = [
   { key: "command", name: "", value: `${appPath} "%1"` },
   { name: "", value: `Open with ${appName}` },
-  { name: "Icon", value: `${appPath}` },
+  { name: "Icon", value: process.execPath },
 ];
+
+const obsoleteFileContextMenuKeys = obsoleteAppNames.map(
+  (name) => `\\Software\\Classes\\*\\shell\\${name}`,
+);
+const obsoleteFolderContextMenuKeys = obsoleteAppNames.map(
+  (name) => `\\Software\\Classes\\Directory\\shell\\${name}`,
+);
+const obsoleteFolderBackgroundContextMenuKeys = obsoleteAppNames.map(
+  (name) => `\\Software\\Classes\\Directory\\background\\shell\\${name}`,
+);
 
 exports.fileContextMenu = new ShellOption(
   `\\Software\\Classes\\*\\shell\\${appName}`,
   contextParts,
+  obsoleteFileContextMenuKeys,
 );
 exports.folderContextMenu = new ShellOption(
   `\\Software\\Classes\\Directory\\shell\\${appName}`,
   contextParts,
+  obsoleteFolderContextMenuKeys,
 );
 exports.folderBackgroundContextMenu = new ShellOption(
   `\\Software\\Classes\\Directory\\background\\shell\\${appName}`,
   JSON.parse(JSON.stringify(contextParts).replace("%1", "%V")),
+  obsoleteFolderBackgroundContextMenuKeys,
 );
